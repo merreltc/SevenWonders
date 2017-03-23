04ab11e5e47a08fdfdafb46506be27794155f613
Carrie Utter <uttercm@rose-hulman.edu>
2017-03-23 10:20:35 -0400

    TDD: testNonDefaultConflictTokens passes.

--- 7_Wonders/src/main/java/dataStructures/ConflictToken.java
+++ 7_Wonders/src/main/java/dataStructures/ConflictToken.java
@@ -5,5 +5,10 @@ public class ConflictToken extends Chip {
 	public ConflictToken() {
 		super();
 		this.setValue(1);
+	}
+
+	public ConflictToken(int value) {
+		super();
+		this.setValue(value);
 	}	
 }
