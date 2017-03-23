3e5f76d985998d6da6ea5fb291488e4b28bc73c4
EricMoorman <moormaet@rose-hulman.edu>
2017-03-22 21:38:35 -0400

    fail testButtonCreation

--- 7_Wonders/src/test/java/tests/gui/ButtonTests.java
+++ 7_Wonders/src/test/java/tests/gui/ButtonTests.java
@@ -1,13 +1,18 @@
 package tests.gui;
 
+import java.awt.Point;
+import java.awt.geom.Point2D;
+
 import org.junit.Test;
 import guiMain.Button;
+import junit.framework.Assert;
 
 public class ButtonTests {
 
 	@Test
 	public void testButtonCreation(){
 		Button button = new Button(100, 100);
-		button.getBounds();
+		Point2D testpoint = new Point(100,100);
+		Assert.assertEquals(testpoint, button.getBounds());
 	}
 }
