3b7e94a7ae307e3a1f9dd07e33ef026de302634e
EricMoorman <moormaet@rose-hulman.edu>
2017-03-22 20:59:51 -0400

    refactored test code to do setup

--- 7_Wonders/src/test/java/tests/gui/GuiStartUpTests.java
+++ 7_Wonders/src/test/java/tests/gui/GuiStartUpTests.java
@@ -1,37 +1,39 @@
 package tests.gui;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import guiMain.*;
 
 public class GuiStartUpTests {
 
+	private GuiMainMenu menu;
+	
+	@Before
+	public void SetUp(){
+		menu = new GuiMainMenu();
+		menu.Start();
+	}
+	
+	
 	@Test
 	public void testMakeFrame(){
-		GuiMainMenu menu = new GuiMainMenu();
-		menu.Start();
 		Assert.assertNotNull(menu.GetFrame());
 	}
 	
 	@Test
 	public void testSize(){
-		GuiMainMenu menu = new GuiMainMenu();
-		menu.Start();
 		Assert.assertEquals(1000, menu.GetFrame().getHeight());
 		Assert.assertEquals(1000, menu.GetFrame().getWidth());
 	}
 	
 	@Test
 	public void testVisible(){
-		GuiMainMenu menu = new GuiMainMenu();
-		menu.Start();
 		Assert.assertTrue(menu.GetFrame().isVisible());
 	}
 	
 	@Test
 	public void testTitle(){
-		GuiMainMenu menu = new GuiMainMenu();
-		menu.Start();
 		Assert.assertEquals("Seven Wonders", menu.GetFrame().getTitle());
 	}
 }
