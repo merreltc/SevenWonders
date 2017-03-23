d28d85045a06cbff2eb3c73e6f9c3807ad2f3684
EricMoorman <moormaet@rose-hulman.edu>
2017-03-22 21:36:18 -0400

    pass testButtonCreation

--- 7_Wonders/src/main/java/guiMain/Button.java
+++ 7_Wonders/src/main/java/guiMain/Button.java
@@ -1,9 +1,15 @@
 package guiMain;
 
+import java.awt.geom.Point2D;
+
 public class Button {
 
 	public Button(int width, int height){
 		
+	}
+	
+	public Point2D getBounds(){
+		return null;
 	}
 	
 }
