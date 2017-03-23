5f7d4e244be509d202ef04607681607efe62a00c
Carrie Utter <uttercm@rose-hulman.edu>
2017-03-23 16:44:20 -0400

    TDD: testTradeFromTo passes.

--- 7_Wonders/src/main/java/backend/TradeHandler.java
+++ 7_Wonders/src/main/java/backend/TradeHandler.java
@@ -4,6 +4,10 @@ import dataStructures.Player;
 
 public class TradeHandler {
 
+	public void tradeFromTo(Player from, Player to, int valueToTrade) {
+		tradeFromToValue1(from, to, 1);
+		tradeFromToValue3(from, to, 1);
+	}	
 	public void tradeFromToValue1(Player from, Player to, int numCoinsToTrade) {
 		from.removeValue1(numCoinsToTrade);
 		to.addValue1(numCoinsToTrade);
@@ -12,5 +16,5 @@ public class TradeHandler {
 	public void tradeFromToValue3(Player from, Player to, int numCoinsToTrade) {
 		from.removeValue3(numCoinsToTrade);
 		to.addValue3(numCoinsToTrade);
-	}	
+	}
 }
