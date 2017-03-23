a50cb9accf98a71e31ca4c82faa803f4902dcb5f
Trinity Merrell <merrelt@rose-hulman.edu>
2017-03-22 21:35:12 -0400

    TDD: testInvalidCoin5 passes

--- 7_Wonders/src/main/java/dataStructures/Coin.java
+++ 7_Wonders/src/main/java/dataStructures/Coin.java
@@ -9,7 +9,7 @@ public class Coin extends Chip {
 	
 	public Coin(int value) {
 		super();
-		if(value == -1 || value == 0 || value == 2 || value == 4) {
+		if(value == -1 || value == 0 || value == 2 || value == 4 || value == 5) {
 			throw new IllegalArgumentException();
 		}
 		
