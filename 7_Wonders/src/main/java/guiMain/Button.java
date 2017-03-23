cacc1886850cf055d298c31bbf33c8130ecd8171
Shijun Yu <y.s.j@live.cn>
2017-03-22 21:46:12 -0400

    passed testButtonCreation2

--- 7_Wonders/src/main/java/guiMain/Button.java
+++ 7_Wonders/src/main/java/guiMain/Button.java
@@ -4,13 +4,14 @@ import java.awt.Point;
 import java.awt.geom.Point2D;
 
 public class Button {
+	private Point point;
 
 	public Button(int width, int height){
-		
+		point = new Point(width, height);
 	}
 	
 	public Point2D getBounds(){
-		return new Point(100,100);
+		return point;
 	}
 	
 }
