3bb21ec4e2c0787c8c52c7e0d546546f544a9b69
Carrie Utter <uttercm@rose-hulman.edu>
2017-03-23 16:50:58 -0400

    TDD: testTradeFromToMultiValue1SingleValue3 passes.

--- 7_Wonders/src/main/java/backend/TradeHandler.java
+++ 7_Wonders/src/main/java/backend/TradeHandler.java
@@ -5,7 +5,11 @@ import dataStructures.Player;
 public class TradeHandler {
 
 	public void tradeFromTo(Player from, Player to, int valueToTrade) {
+		if(valueToTrade == 4){
 		tradeFromToValue1(from, to, 1);
+		}else{
+			tradeFromToValue1(from, to, 2);
+		}
 		tradeFromToValue3(from, to, 1);
 	}	
 	public void tradeFromToValue1(Player from, Player to, int numCoinsToTrade) {
