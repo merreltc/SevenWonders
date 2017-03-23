f586ece28f60105f013339677e346d9b8d76abd0
Shijun Yu <y.s.j@live.cn>
2017-03-22 21:48:03 -0400

    failed testButtonCreationPosition

--- 7_Wonders/src/test/java/tests/gui/ButtonTests.java
+++ 7_Wonders/src/test/java/tests/gui/ButtonTests.java
@@ -22,4 +22,9 @@ public class ButtonTests {
 		Point2D testpoint = new Point(50,50);
 		Assert.assertEquals(testpoint, button.getBounds());
 	}
+	
+	@Test
+	public void testButtonCreationPosition(){
+		Button button = new Button(250, 250, 100, 100);
+	}
 }
