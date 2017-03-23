646a2b3191dcb61f8996deaa17be4b76a543cd30
EricMoorman <moormaet@rose-hulman.edu>
2017-03-22 20:48:41 -0400

    pass testSize

--- 7_Wonders/src/main/java/guiMain/GuiMainMenu.java
+++ 7_Wonders/src/main/java/guiMain/GuiMainMenu.java
@@ -4,14 +4,15 @@ import javax.swing.JFrame;
 
 public class GuiMainMenu {
 
+	private JFrame frame;
 	
 	public void Start(){
-		
-		
+		frame = new JFrame();
+		frame.setSize(1000, 1000);
 	}
 	
 	public JFrame GetFrame() {
-		return new JFrame();
+		return frame;
 	}
 	
 }
