efa3cabdf9002e5cb512e93111c2fe5e1fab0cee
Carrie Utter <uttercm@rose-hulman.edu>
2017-03-23 15:09:49 -0400

    TDD: failing test for number of value 1 coins added = -1 (Invalid)

--- 7_Wonders/src/test/java/DataStructureTests/PlayerTest.java
+++ 7_Wonders/src/test/java/DataStructureTests/PlayerTest.java
@@ -11,44 +11,52 @@ public class PlayerTest {
 	@Test
 	public void testDefaultPlayer() {
 		Player player = new Player();
-		
+
 		assertEquals(3, player.getCoinTotal());
 		assertEquals(0, player.getConflictTotal());
 		assertEquals("Jane Doe", player.getName());
 	}
-	
+
 	@Test
-	public void testDefaultPlayerCoins(){
+	public void testDefaultPlayerCoins() {
 		Player player = new Player();
-		
+
 		assertEquals(3, player.getNumValue1Coins());
 		assertEquals(0, player.getNumValue3Coins());
 	}
-	
+
 	@Test
-	public void testAddValue1Coins(){
+	public void testAddValue1Coins() {
 		Player player = new Player();
-		
+
 		player.addValue1(1);
 		assertEquals(4, player.getCoinTotal());
 		assertEquals(4, player.getNumValue1Coins());
-		
+
 		player.addValue1(3);
 		assertEquals(7, player.getCoinTotal());
 		assertEquals(7, player.getNumValue1Coins());
 	}
-	
+
 	@Test
-	public void testAddValue3Coins(){
+	public void testAddValue3Coins() {
 		Player player = new Player();
-		
+
 		player.addValue3(1);
 		assertEquals(6, player.getCoinTotal());
 		assertEquals(1, player.getNumValue3Coins());
-		
+
 		player.addValue3(3);
 		assertEquals(15, player.getCoinTotal());
 		assertEquals(4, player.getNumValue3Coins());
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testAddInvalidNumValue1CoinsNeg1() {
+		Player player = new Player();
+		
+		player.addValue1(-1);
+		fail();
 	}
 
 }
