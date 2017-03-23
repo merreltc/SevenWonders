be5e96d1770593f8e0ae0bab9e36428f297145de
Carrie Utter <uttercm@rose-hulman.edu>
2017-03-23 16:25:59 -0400

    TDD: trade tests now all pass.

--- 7_Wonders/src/main/java/backend/TradeHandler.java
+++ 7_Wonders/src/main/java/backend/TradeHandler.java
@@ -4,9 +4,8 @@ import dataStructures.Player;
 
 public class TradeHandler {
 
-	public void tradeFromTo(Player player1, Player player2, int valueToTrade) {
+	public void tradeFromToValue1(Player player1, Player player2, int valueToTrade) {
 		player2.addValue1(valueToTrade);
 		player1.removeValue1(valueToTrade);
-	}
-	
+	}	
 }
