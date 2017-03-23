c089fb0580a5926d6fd197d8386a06bef186d8d3
Carrie Utter <uttercm@rose-hulman.edu>
2017-03-23 16:30:11 -0400

    TDD: refactored tradeHandler. all tests pass.

--- 7_Wonders/src/main/java/backend/TradeHandler.java
+++ 7_Wonders/src/main/java/backend/TradeHandler.java
@@ -4,13 +4,13 @@ import dataStructures.Player;
 
 public class TradeHandler {
 
-	public void tradeFromToValue1(Player player1, Player player2, int valueToTrade) {
-		player2.addValue1(valueToTrade);
-		player1.removeValue1(valueToTrade);
+	public void tradeFromToValue1(Player from, Player to, int valueToTrade) {
+		from.removeValue1(valueToTrade);
+		to.addValue1(valueToTrade);
 	}
 
-	public void tradeFromToValue3(Player player1, Player player2, int valueToTrade) {
-		player2.addValue3(valueToTrade);
-		player1.removeValue3(valueToTrade);
+	public void tradeFromToValue3(Player from, Player to, int valueToTrade) {
+		from.removeValue3(valueToTrade);
+		to.addValue3(valueToTrade);
 	}	
 }
