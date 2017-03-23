99d196132f7688902e5af83d136726aae017efa0
Carrie Utter <uttercm@rose-hulman.edu>
2017-03-23 16:29:00 -0400

    TDD: testSingleTradeValue3Coins passes.

--- 7_Wonders/src/main/java/backend/TradeHandler.java
+++ 7_Wonders/src/main/java/backend/TradeHandler.java
@@ -7,5 +7,10 @@ public class TradeHandler {
 	public void tradeFromToValue1(Player player1, Player player2, int valueToTrade) {
 		player2.addValue1(valueToTrade);
 		player1.removeValue1(valueToTrade);
+	}
+
+	public void tradeFromToValue3(Player player1, Player player2, int valueToTrade) {
+		player2.addValue3(valueToTrade);
+		player1.removeValue3(valueToTrade);
 	}	
 }
