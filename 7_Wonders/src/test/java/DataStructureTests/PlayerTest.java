a9c2edab07d9de0bc94a91a4ec683dec714f8621
Carrie Utter <uttercm@rose-hulman.edu>
2017-03-23 15:01:53 -0400

    TDD: failing test for adding specified value 1 coins.

--- 7_Wonders/src/test/java/DataStructureTests/PlayerTest.java
+++ 7_Wonders/src/test/java/DataStructureTests/PlayerTest.java
@@ -18,11 +18,24 @@ public class PlayerTest {
 	}
 	
 	@Test
-	public void testPlayerCoins(){
+	public void testDefaultPlayerCoins(){
 		Player player = new Player();
 		
 		assertEquals(3, player.getNumValue1Coins());
 		assertEquals(0, player.getNumValue3Coins());
+	}
+	
+	@Test
+	public void testAddValue1Coins(){
+		Player player = new Player();
+		
+		player.addValue1(1);
+		assertEquals(4, player.getCoinTotal());
+		assertEquals(4, player.getNumValue1Coins());
+		
+		player.addValue1(3);
+		assertEquals(7, player.getCoinTotal());
+		assertEquals(7, player.getNumValue1Coins());
 	}
 
 }
