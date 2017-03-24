39927ccf8642bc13a3a943c315a1518a037c9bce
Trinity Merrell <merrelt@rose-hulman.edu>
2017-03-24 19:56:12 -0400

    TDD: refactored player passes all tests

--- 7_Wonders/src/main/java/dataStructures/Player.java
+++ 7_Wonders/src/main/java/dataStructures/Player.java
@@ -3,41 +3,38 @@ package dataStructures;
 import exceptions.InsufficientFundsException;
 
 public class Player {
+	private String name = "Jane Doe";
+	
 	private int coinTotal = 3;
-	private int conflictTotal = 0;
 	private int numOfValue1Coins = 3;
 	private int numOfValue3Coins = 0;
-	private String name = "Jane Doe";
+	
+	private int conflictTotal = 0;
 
 	private enum CoinType {
 		ONE, THREE
 	}
 
-	public String getName() {
-		return this.name;
-	}
-
-	public int getCoinTotal() {
-		return this.coinTotal;
-	}
-
-	public int getConflictTotal() {
-		return this.conflictTotal;
-	}
-
-	public int getNumValue1Coins() {
-		return this.numOfValue1Coins;
-	}
-
-	public int getNumValue3Coins() {
-		return this.numOfValue3Coins;
-	}
-
 	public void addValue1(int numCoinsToAdd) {
-		validateNumCoinsToAdd(numCoinsToAdd, 47);
+		int maxValue1CoinsInGame = 46;
+		validateNumCoinsToAdd(numCoinsToAdd, maxValue1CoinsInGame);
 
 		this.coinTotal += numCoinsToAdd;
 		this.numOfValue1Coins += numCoinsToAdd;
+	}
+
+	public void addValue3(int numCoinsToAdd) {
+		int maxValue3CoinsInGame = 24;
+		validateNumCoinsToAdd(numCoinsToAdd, maxValue3CoinsInGame);
+
+		this.coinTotal += 3 * numCoinsToAdd;
+		this.numOfValue3Coins += numCoinsToAdd;
+	}
+
+	private void validateNumCoinsToAdd(int numCoins, int max) {
+		if (numCoins <= -1 || numCoins > max) {
+			throw new IllegalArgumentException();
+		}
 	}
 
 	public void removeValue1(int numCoinsToRemove) {
@@ -46,27 +43,14 @@ public class Player {
 		this.coinTotal -= numCoinsToRemove;
 		this.numOfValue1Coins -= numCoinsToRemove;
 	}
-
-	public void addValue3(int numCoinsToAdd) {
-		validateNumCoinsToAdd(numCoinsToAdd, 25);
-
-		this.coinTotal += 3 * numCoinsToAdd;
-		this.numOfValue3Coins += numCoinsToAdd;
-	}
-
-	private void validateNumCoinsToAdd(int numCoins, int max) {
-		if (numCoins <= -1 || numCoins >= max) {
-			throw new IllegalArgumentException();
-		}
-	}
-
+	
 	public void removeValue3(int numCoinsToRemove) {
 		validateNumCoinsToRemove(numCoinsToRemove, CoinType.THREE);
 
 		this.coinTotal -= 3 * numCoinsToRemove;
 		this.numOfValue3Coins -= numCoinsToRemove;
 	}
-
+	
 	private void validateNumCoinsToRemove(int numCoins, CoinType type) {
 		if (numCoins <= -1) {
 			throw new IllegalArgumentException();
@@ -85,5 +69,25 @@ public class Player {
 		}
 
 		return this.numOfValue3Coins;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+
+	public int getCoinTotal() {
+		return this.coinTotal;
+	}
+
+	public int getNumValue1Coins() {
+		return this.numOfValue1Coins;
+	}
+
+	public int getNumValue3Coins() {
+		return this.numOfValue3Coins;
+	}
+	
+	public int getConflictTotal() {
+		return this.conflictTotal;
 	}
 }
