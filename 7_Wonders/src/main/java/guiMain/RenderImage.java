d0804764005a09d61a2a50fdf29226cf02809baa
EricMoorman <moormaet@rose-hulman.edu>
2017-03-24 16:16:39 -0400

    Added music and image rendering capabilities

---
+++ 7_Wonders/src/main/java/guiMain/RenderImage.java
@@ -0,0 +1,33 @@
+package guiMain;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class RenderImage {
+
+	private Image image;
+	private JPanel panel;
+	private Graphics g;
+	
+	public RenderImage(){
+	}
+	
+	public void draw(Graphics g, String name, int x, int y, int width, int height){
+		BufferedImage img = null;
+		try {
+			/*TODO Add correct File path to the below command*/
+		    img = ImageIO.read(new File("images\\" + name + ".png"));
+		    g.drawImage(img, x, y, width, height, null);
+		} catch (IOException e) {
+			System.out.println("Fail");
+		}
+		
+	}
+	
+}
