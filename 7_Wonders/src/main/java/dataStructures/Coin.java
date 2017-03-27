5c80aba477aeca0e8a115e1d23433296899396a1
Carrie Utter <uttercm@rose-hulman.edu>
2017-03-27 18:23:38 -0400

    TDD: testInvalidCoin0ErrorMessage passes.

--- 7_Wonders/src/main/java/dataStructures/Coin.java
+++ 7_Wonders/src/main/java/dataStructures/Coin.java
@@ -10,7 +10,7 @@ public class Coin extends Chip {
 	public Coin(int value) {
 		super();
 		if(value <= 0 || value == 2 || value >= 4) {
-			throw new IllegalArgumentException("Cannot have a coin whose's value is -1");
+			throw new IllegalArgumentException("Cannot have a coin whose's value is " + value);
 		}
 		
 		this.setValue(value);
