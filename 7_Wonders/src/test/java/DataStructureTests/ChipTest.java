b985e7856b9be78dbb19bf5c01f3e678882d0406
Carrie Utter <uttercm@rose-hulman.edu>
2017-03-27 18:20:07 -0400

    TDD: failing test for invalid coin value = -1 illegal argument exception message.

--- 7_Wonders/src/test/java/DataStructureTests/ChipTest.java
+++ 7_Wonders/src/test/java/DataStructureTests/ChipTest.java
@@ -13,19 +13,19 @@ public class ChipTest {
 	@Test
 	public void testDefaultCoin() {
 		Chip coin = new Coin();
-		
+
 		assertEquals(1, coin.getValue());
 	}
-	
+
 	@Test
 	public void testNonDefaultCoin() {
 		Chip coinValue1 = new Coin(1);
 		assertEquals(1, coinValue1.getValue());
-		
+
 		Chip coinValue3 = new Coin(3);
 		assertEquals(3, coinValue3.getValue());
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testInvalidCoinNeg1() {
 		Chip coinValueNeg1 = new Coin(-1);
@@ -37,74 +37,84 @@ public class ChipTest {
 		Chip coinValue0 = new Coin(0);
 		fail();
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testInvalidCoin2() {
 		Chip coinValue2 = new Coin(2);
 		fail();
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testInvalidCoin4() {
 		Chip coinValue4 = new Coin(4);
 		fail();
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testInvalidCoin5() {
 		Chip coinValue5 = new Coin(5);
 		fail();
 	}
-	
+
 	@Test
-	public void testDefaultConflictToken(){
+	public void testDefaultConflictToken() {
 		Chip conflictToken = new ConflictToken();
-		
+
 		assertEquals(1, conflictToken.getValue());
 	}
-	
+
 	@Test
-	public void testNonDefaultConflictTokens(){
+	public void testNonDefaultConflictTokens() {
 		Chip conTokenNeg1 = new ConflictToken(-1);
 		assertEquals(-1, conTokenNeg1.getValue());
-		
+
 		Chip conTokenValue1 = new ConflictToken(1);
 		assertEquals(1, conTokenValue1.getValue());
-		
+
 		Chip conTokenValue3 = new ConflictToken(3);
 		assertEquals(3, conTokenValue3.getValue());
-		
+
 		Chip conTokenValue5 = new ConflictToken(5);
 		assertEquals(5, conTokenValue5.getValue());
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
-	public void testInvalidConflictTokenNeg2(){
+	public void testInvalidConflictTokenNeg2() {
 		Chip conTokenNeg2 = new ConflictToken(-2);
 		fail();
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
-	public void testInvalidConflictToken0(){
+	public void testInvalidConflictToken0() {
 		Chip conToken0 = new ConflictToken(0);
 		fail();
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
-	public void testInvalidConflictToken2(){
+	public void testInvalidConflictToken2() {
 		Chip conToken0 = new ConflictToken(2);
 		fail();
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
-	public void testInvalidConflictToken4(){
+	public void testInvalidConflictToken4() {
 		Chip conToken0 = new ConflictToken(4);
 		fail();
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
-	public void testInvalidConflictToken6(){
+	public void testInvalidConflictToken6() {
 		Chip conToken0 = new ConflictToken(6);
 		fail();
+	}
+
+	@Test
+	public void testInvalidCoinNeg1ErrorMessage() {
+		try{
+			Chip coin = new Coin(-1);
+		}catch(IllegalArgumentException error){
+			String message = "Cannot have a coin whose's value is -1";
+			assertEquals(message, error.getMessage());
+		}
 	}
 }
