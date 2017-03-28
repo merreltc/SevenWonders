e1195edbce8d692b409b1004a261b463455d7b52
Carrie Utter <uttercm@rose-hulman.edu>
2017-03-28 15:53:33 -0400

    TDD: testGameManagerGetMinPlayers passes.

---
+++ 7_Wonders/src/main/java/backend/GameManager.java
@@ -0,0 +1,12 @@
+package backend;
+
+public class GameManager {
+	
+	public GameManager(int numPlayers){
+		
+	}
+	
+	public int getPlayerNum() {
+		return 3;
+	}
+}
--- 7_Wonders/src/test/java/backendTests/GameManagerTest.java
+++ 7_Wonders/src/test/java/backendTests/GameManagerTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import backend.GameManager;
+
 public class GameManagerTest {
 
 	@Test
