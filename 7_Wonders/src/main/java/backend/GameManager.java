734e6f76653f9250780e519cc108aaaa4eef1663
Carrie Utter <uttercm@rose-hulman.edu>
2017-03-28 15:58:42 -0400

    TDD: testGameManagerGetMaxPlayers passes.

--- 7_Wonders/src/main/java/backend/GameManager.java
+++ 7_Wonders/src/main/java/backend/GameManager.java
@@ -1,12 +1,13 @@
 package backend;
 
 public class GameManager {
+	private int numPlayers = 3;
 	
 	public GameManager(int numPlayers){
-		
+		this.numPlayers = numPlayers;
 	}
 	
 	public int getPlayerNum() {
-		return 3;
+		return this.numPlayers;
 	}
 }
