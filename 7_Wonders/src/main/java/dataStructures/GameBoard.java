bab8195a89b8df4097bb662591308f904dc8efec
Carrie Utter <uttercm@rose-hulman.edu>
2017-03-28 15:30:47 -0400

    TDD: testGameBoardGetPlayers passes.

--- 7_Wonders/src/main/java/dataStructures/GameBoard.java
+++ 7_Wonders/src/main/java/dataStructures/GameBoard.java
@@ -4,15 +4,21 @@ import java.util.ArrayList;
 
 public class GameBoard {
 	private int numPlayers = 0;
+	private ArrayList<Player> players = new ArrayList<Player>();
 	
 	public GameBoard(){
 	}
 	
 	public GameBoard(ArrayList<Player> players) {
 		this.numPlayers = players.size();
+		this.players = players;
 	}
 
 	public int getNumPlayers(){
 		return this.numPlayers;
+	}
+
+	public ArrayList<Player> getPlayers() {
+		return this.players;
 	}
 }
