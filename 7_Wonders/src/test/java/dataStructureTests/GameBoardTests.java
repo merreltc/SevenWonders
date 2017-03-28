bdac6da9b305cc5f3f4d7ab5f28e97f0c6675978
Carrie Utter <uttercm@rose-hulman.edu>
2017-03-28 15:21:48 -0400

    TDD: failing test for GameBoard get num min players = 3

--- 7_Wonders/src/test/java/dataStructureTests/GameBoardTests.java
+++ 7_Wonders/src/test/java/dataStructureTests/GameBoardTests.java
@@ -2,9 +2,12 @@ package dataStructureTests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import dataStructures.GameBoard;
+import dataStructures.Player;
 
 public class GameBoardTests {
 
@@ -13,6 +16,18 @@ public class GameBoardTests {
 		GameBoard board = new GameBoard();
 		
 		assertEquals(0, board.getNumPlayers());
+	}
+	
+	@Test
+	public void testGameBoardMinPlayers(){
+		ArrayList<Player> players = new ArrayList<Player>();
+		players.add(new Player());
+		players.add(new Player());
+		players.add(new Player());
+		
+		GameBoard board = new GameBoard(players);
+		
+		assertEquals(3, board.getNumPlayers());
 	}
 
 }
