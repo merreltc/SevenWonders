6d3ba39915f4de6eab25d3a762bf59e29ea038ae
Carrie Utter <uttercm@rose-hulman.edu>
2017-03-28 16:23:40 -0400

    TDD: testCreateGameBoardDefault passes

--- 7_Wonders/src/main/java/backend/SetUpHandler.java
+++ 7_Wonders/src/main/java/backend/SetUpHandler.java
@@ -1,16 +1,26 @@
 package backend;
 
+import dataStructures.GameBoard;
+
 public class SetUpHandler {
 	private int playerNum;
-	
+
+	public GameBoard setUp(int numPlayers) {
+		setPlayerNum(numPlayers);
+		return createGameBoard();
+	}
 
 	public void setPlayerNum(int num) {
-		if(num < 3 || num > 7)
+		if (num < 3 || num > 7)
 			throw new IllegalArgumentException("Cannot play with " + num + " players");
 		playerNum = num;
-		
+
 	}
-	
+
+	public GameBoard createGameBoard() {
+		return new GameBoard();
+	}
+
 	public int getPlayerNum() {
 		return this.playerNum;
 	}
