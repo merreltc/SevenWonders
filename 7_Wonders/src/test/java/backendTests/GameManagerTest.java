e78554b0d35d7ee20a8d78bce555a4d0395aeb13
Carrie Utter <uttercm@rose-hulman.edu>
2017-03-28 17:15:20 -0400

    TDD: failing test for default set up get game board

--- 7_Wonders/src/test/java/backendTests/GameManagerTest.java
+++ 7_Wonders/src/test/java/backendTests/GameManagerTest.java
@@ -6,33 +6,42 @@ import org.easymock.EasyMock;
 import org.junit.Test;
 
 import backend.GameManager;
+import dataStructures.GameBoard;
 
 public class GameManagerTest {
 
 	@Test
 	public void testGameManagerGetMinPlayers() {
 		GameManager manager = new GameManager(3);
-		
+
 		assertEquals(3, manager.getPlayerNum());
 	}
-	
+
 	@Test
 	public void testGameManagerGetMaxPlayers() {
 		GameManager manager = new GameManager(7);
-		
+
 		assertEquals(7, manager.getPlayerNum());
 	}
 
 	@Test
 	public void testDefaultSetUp() {
 		GameManager manager = EasyMock.createMockBuilder(GameManager.class).withConstructor(3).createMock();
-		
+
 		manager.setUpGame();
-	
+
 		EasyMock.replay(manager);
-		
+
 		EasyMock.verify(manager);
+	}
+	
+	@Test
+	public void testDefaultSetUpGetGameBoard() {
+		GameManager manager = new GameManager(3);
 		
+		GameBoard board = manager.getGameBoard();
+		
+		assertEquals(new GameBoard(), board);
 		
 	}
 }
