5200ef8d77bf581308d584536f17df13cd5a3644
Carrie Utter <uttercm@rose-hulman.edu>
2017-03-28 17:09:36 -0400

    TDD: failing modified default game board test

--- 7_Wonders/src/test/java/dataStructureTests/GameBoardTests.java
+++ 7_Wonders/src/test/java/dataStructureTests/GameBoardTests.java
@@ -13,8 +13,14 @@ public class GameBoardTests {
 
 	@Test
 	public void testDefaultGameBoard() {
+		ArrayList<Player> players = new ArrayList<Player>();
+		players.add(new Player());
+		players.add(new Player());
+		players.add(new Player());
+
 		GameBoard board = new GameBoard();
 
+		assertEquals(3, board.getPlayers().size());
 		assertEquals(3, board.getNumPlayers());
 	}
 
