1296ed72b54d3c145aa35e20b8184d1381aee7e2
Trinity Merrell <merrelt@rose-hulman.edu>
2017-03-28 14:49:09 -0400

    TDD: failing test for removing value 1 coins = 4, with default player

--- 7_Wonders/src/test/java/DataStructureTests/PlayerTest.java
+++ 7_Wonders/src/test/java/DataStructureTests/PlayerTest.java
@@ -256,161 +256,173 @@ public class PlayerTest {
 		Player player2 = new Player();
 
 		player1.addValue3(2);
-		
+
 		TradeHandler.tradeFromTo(player1, player2, 4);
-		
+
 		assertEquals(5, player1.getCoinTotal());
 		assertEquals(2, player1.getNumValue1Coins());
 		assertEquals(1, player1.getNumValue3Coins());
-		
+
 		assertEquals(7, player2.getCoinTotal());
 		assertEquals(4, player2.getNumValue1Coins());
 		assertEquals(1, player2.getNumValue3Coins());
 	}
-	
+
 	@Test
 	public void testTradeFromToMultiValue1SingleValue3() {
 		Player player1 = new Player();
 		Player player2 = new Player();
-		
+
 		player1.addValue3(2);
-		
+
 		TradeHandler.tradeFromTo(player1, player2, 5);
-		
+
 		assertEquals(4, player1.getCoinTotal());
 		assertEquals(1, player1.getNumValue1Coins());
 		assertEquals(1, player1.getNumValue3Coins());
-		
+
 		assertEquals(8, player2.getCoinTotal());
 		assertEquals(5, player2.getNumValue1Coins());
 		assertEquals(1, player2.getNumValue3Coins());
 	}
-	
+
 	@Test
 	public void testTradeFromToSingleValue1MultiValue3() {
 		Player player1 = new Player();
 		Player player2 = new Player();
-		
+
 		player1.addValue3(2);
-		
+
 		TradeHandler.tradeFromTo(player1, player2, 7);
-		
+
 		assertEquals(2, player1.getCoinTotal());
 		assertEquals(2, player1.getNumValue1Coins());
 		assertEquals(0, player1.getNumValue3Coins());
-		
+
 		assertEquals(10, player2.getCoinTotal());
 		assertEquals(4, player2.getNumValue1Coins());
 		assertEquals(2, player2.getNumValue3Coins());
 	}
-	
+
 	@Test
 	public void testTradeFromToMultiValue1MultiValue3() {
 		Player player1 = new Player();
 		Player player2 = new Player();
-		
+
 		player1.addValue3(2);
-		
+
 		TradeHandler.tradeFromTo(player1, player2, 8);
-		
+
 		assertEquals(1, player1.getCoinTotal());
 		assertEquals(1, player1.getNumValue1Coins());
 		assertEquals(0, player1.getNumValue3Coins());
-		
+
 		assertEquals(11, player2.getCoinTotal());
 		assertEquals(5, player2.getNumValue1Coins());
 		assertEquals(2, player2.getNumValue3Coins());
 	}
-	
+
 	@Test
 	public void testTradeFromToSufficientValue1NoValue3() {
 		Player player1 = new Player();
 		Player player2 = new Player();
-		
+
 		TradeHandler.tradeFromTo(player1, player2, 3);
-		
+
 		assertEquals(0, player1.getCoinTotal());
 		assertEquals(0, player1.getNumValue1Coins());
 		assertEquals(0, player1.getNumValue3Coins());
-		
+
 		assertEquals(6, player2.getCoinTotal());
 		assertEquals(6, player2.getNumValue1Coins());
 		assertEquals(0, player2.getNumValue3Coins());
 	}
-	
+
 	@Test
 	public void testTradeFromToGreaterValue1ThanValue3() {
 		Player player1 = new Player();
 		Player player2 = new Player();
-		
+
 		player1.addValue3(1);
 		TradeHandler.tradeFromTo(player1, player2, 6);
-		
+
 		assertEquals(0, player1.getCoinTotal());
 		assertEquals(0, player1.getNumValue1Coins());
 		assertEquals(0, player1.getNumValue3Coins());
-		
+
 		assertEquals(9, player2.getCoinTotal());
 		assertEquals(6, player2.getNumValue1Coins());
 		assertEquals(1, player2.getNumValue3Coins());
 	}
-	
+
 	@Test
 	public void testAddInvalidNumValue1CoinsNeg1ErrorMessage() {
 		Player player = new Player();
 
-		try{
-		player.addValue1(-1);
-		}catch(IllegalArgumentException error){
+		try {
+			player.addValue1(-1);
+		} catch (IllegalArgumentException error) {
 			String message = "Cannot add -1 value 1 coins";
 			assertEquals(message, error.getMessage());
 		}
 	}
-	
+
 	@Test
 	public void testAddInvalidNumValue1Coins47ErrorMessage() {
 		Player player = new Player();
 
-		try{
-		player.addValue1(47);
-		}catch(IllegalArgumentException error){
+		try {
+			player.addValue1(47);
+		} catch (IllegalArgumentException error) {
 			String message = "Cannot add 47 value 1 coins";
 			assertEquals(message, error.getMessage());
 		}
 	}
-	
+
 	@Test
 	public void testAddInvalidNumValue3CoinsNeg1ErrorMessage() {
 		Player player = new Player();
 
-		try{
-		player.addValue3(-1);
-		}catch(IllegalArgumentException error){
+		try {
+			player.addValue3(-1);
+		} catch (IllegalArgumentException error) {
 			String message = "Cannot add -1 value 3 coins";
 			assertEquals(message, error.getMessage());
 		}
 	}
-	
+
 	@Test
 	public void testInvalidRemoveNumValue1CoinsNeg1ErrorMessage() {
 		Player player = new Player();
-		
-		try{
-		player.removeValue1(-1);
-		}catch (IllegalArgumentException error){
+
+		try {
+			player.removeValue1(-1);
+		} catch (IllegalArgumentException error) {
 			String message = "Cannot remove -1 value 1 coins";
 			assertEquals(message, error.getMessage());
 		}
 	}
-	
+
 	@Test
 	public void testInvalidRemoveNumValue1CoinsNeg2ErrorMessage() {
 		Player player = new Player();
-		
-		try{
-		player.removeValue1(-2);
-		}catch (IllegalArgumentException error){
+
+		try {
+			player.removeValue1(-2);
+		} catch (IllegalArgumentException error) {
 			String message = "Cannot remove -2 value 1 coins";
+			assertEquals(message, error.getMessage());
+		}
+	}
+
+	@Test
+	public void testInsufficientFundsForRemoveValue1CoinsErrorMessage() {
+		Player player = new Player();
+
+		try {
+			player.removeValue1(4);
+		} catch (InsufficientFundsException error) {
+			String message = "Player does not have 4 value 1 coins";
 			assertEquals(message, error.getMessage());
 		}
 	}
