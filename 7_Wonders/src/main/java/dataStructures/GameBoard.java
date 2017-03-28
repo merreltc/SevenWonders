a0bae46aae634a6de3a31a570ba5d27ab1e510c5
Carrie Utter <uttercm@rose-hulman.edu>
2017-03-28 15:23:22 -0400

    TDD: testGameBoardMinPlayers passes.

--- 7_Wonders/src/main/java/dataStructures/GameBoard.java
+++ 7_Wonders/src/main/java/dataStructures/GameBoard.java
@@ -1,8 +1,18 @@
 package dataStructures;
 
+import java.util.ArrayList;
+
 public class GameBoard {
+	private int numPlayers = 0;
 	
+	public GameBoard(){
+	}
+	
+	public GameBoard(ArrayList<Player> players) {
+		this.numPlayers = 3;
+	}
+
 	public int getNumPlayers(){
-		return 0;
+		return this.numPlayers;
 	}
 }
