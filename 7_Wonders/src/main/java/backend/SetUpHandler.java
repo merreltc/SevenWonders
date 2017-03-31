94475a13852d1a15a334a3d195b91c37cf0e2658
Trinity Merrell <merrelt@rose-hulman.edu>
2017-03-31 14:34:36 -0400

    TDD: SetUpHandler refactored. All tests pass

--- 7_Wonders/src/main/java/backend/SetUpHandler.java
+++ 7_Wonders/src/main/java/backend/SetUpHandler.java
@@ -6,8 +6,6 @@ import dataStructures.GameBoard;
 import dataStructures.Player;
 
 public class SetUpHandler {
-	private static int playerNum;
-
 	public static ArrayList<String> setUp(ArrayList<String> playerNames) {
 		validatePlayerNum(playerNames.size());
 		return playerNames;
@@ -17,19 +15,7 @@ public class SetUpHandler {
 		if (num < 3 || num > 7) {
 			throw new IllegalArgumentException("Cannot play with " + num + " players");
 		}
-		
-		playerNum = num;
 
 		return true;
-	}
-
-	public static GameBoard createDefaultGameBoard() {
-		ArrayList<Player> players = new ArrayList<Player>();
-
-		for (int i = 0; i < playerNum; i++) {
-			players.add(new Player());
-		}
-
-		return new GameBoard(players);
 	}
 }
