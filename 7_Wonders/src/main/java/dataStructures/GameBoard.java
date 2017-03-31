59167f6569921ab4257972d9cedd858a3abaa2b5
Carrie Utter <uttercm@rose-hulman.edu>
2017-03-31 16:24:56 -0400

    TDD: testGameGetPreviousPlayerOnRotateClockwiseMax passes.

--- 7_Wonders/src/main/java/dataStructures/GameBoard.java
+++ 7_Wonders/src/main/java/dataStructures/GameBoard.java
@@ -3,12 +3,18 @@ package dataStructures;
 import java.util.ArrayList;
 
 public class GameBoard {
-	private int numPlayers = 3;
+	private int numPlayers;
+	private int previousPlayerIndex;
 	private ArrayList<Player> players = new ArrayList<Player>();
 	
 	public GameBoard(ArrayList<Player> players) {
 		this.numPlayers = players.size();
+		this.previousPlayerIndex = this.numPlayers - 1;
 		this.players = players;
+	}
+	
+	public void rotateClockwise() {
+		this.previousPlayerIndex = 0;
 	}
 
 	public int getNumPlayers(){
@@ -36,8 +42,6 @@ public class GameBoard {
 	}
 
 	public Player getPreviousPlayer() {
-		int previousPlayerIndex = this.players.size() - 1;
-		return this.getPlayer(previousPlayerIndex);
+		return this.getPlayer(this.previousPlayerIndex);
 	}
-
 }
