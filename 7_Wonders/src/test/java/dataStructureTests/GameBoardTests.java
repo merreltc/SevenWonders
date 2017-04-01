ee731893fb1591411a72bf243fbb28c86b91f05b
Trinity Merrell <merrelt@rose-hulman.edu>
2017-04-01 17:04:09 -0400

    TDD: failing test for get player position rotate c-clockwise twice

--- 7_Wonders/src/test/java/dataStructureTests/GameBoardTests.java
+++ 7_Wonders/src/test/java/dataStructureTests/GameBoardTests.java
@@ -217,4 +217,22 @@ public class GameBoardTests {
 		assertEquals(players.get(5), board.getNextPlayer());
 		assertEquals(players.get(0), board.getPreviousPlayer());
 	}
+	
+	@Test
+	public void testGameGetPlayerPositionsOnRotateCounterClockwiseTwice() {
+		ArrayList<Player> players = new ArrayList<Player>();
+		players.add(new Player("Wolverine"));
+		players.add(new Player("Captain America"));
+		players.add(new Player("Black Widow"));
+		players.add(new Player("Hulk"));
+		players.add(new Player("Iron Man"));
+
+		GameBoard board = new GameBoard(players);
+		board.rotateCounterClockwise();
+		board.rotateCounterClockwise();
+
+		assertEquals(players.get(5), board.getCurrentPlayer());
+		assertEquals(players.get(6), board.getNextPlayer());
+		assertEquals(players.get(4), board.getPreviousPlayer());
+	}
 }
