74aac0af89ad2765839f32c00d57df6822114f83
Trinity Merrell <merrelt@rose-hulman.edu>
2017-04-01 17:46:32 -0400

    TDD: refactored rotate tests to include a comparePlayerPosition method

--- 7_Wonders/src/test/java/dataStructureTests/GameBoardTests.java
+++ 7_Wonders/src/test/java/dataStructureTests/GameBoardTests.java
@@ -20,7 +20,6 @@ public class GameBoardTests {
 		players.add(new Player());
 		GameBoard board = new GameBoard(players);
 
-		
 		assertEquals(3, board.getNumPlayers());
 	}
 
@@ -51,7 +50,7 @@ public class GameBoardTests {
 
 		assertEquals(players, board.getPlayers());
 	}
-	
+
 	@Test
 	public void testGameBoardGetPlayerByIndex() {
 		ArrayList<Player> players = new ArrayList<Player>();
@@ -61,11 +60,11 @@ public class GameBoardTests {
 
 		GameBoard board = new GameBoard(players);
 
-		for(int i = 0; i < players.size(); i++) {
+		for (int i = 0; i < players.size(); i++) {
 			assertEquals(players.get(i), board.getPlayer(i));
 		}
 	}
-	
+
 	@Test
 	public void testGameBoardGetPlayerCoinTotalOnStart() {
 		ArrayList<Player> players = new ArrayList<Player>();
@@ -75,11 +74,11 @@ public class GameBoardTests {
 
 		GameBoard board = new GameBoard(players);
 
-		for(int i = 0; i < players.size(); i++) {
+		for (int i = 0; i < players.size(); i++) {
 			assertEquals(3, board.getPlayerCoinTotal(i));
 		}
 	}
-	
+
 	@Test
 	public void testChangeRotateDirectionAndResetPositionsMin() {
 		ArrayList<Player> players = new ArrayList<Player>();
@@ -88,19 +87,14 @@ public class GameBoardTests {
 		players.add(new Player("Black Widow"));
 
 		GameBoard board = new GameBoard(players);
-		board.changeRotateDirectionAndResetPositions(Direction.CLOCKWISE);
-
-		assertEquals(players.get(0), board.getCurrentPlayer());
-		assertEquals(players.get(1), board.getNextPlayer());
-		assertEquals(players.get(2), board.getPreviousPlayer());
 		
-		board.changeRotateDirectionAndResetPositions(Direction.COUNTERCLOCKWISE);
+		board.changeRotateDirectionAndResetPositions(Direction.CLOCKWISE);
+		comparePlayerPositions(players, board, 0, 1, 2);
 
-		assertEquals(players.get(0), board.getCurrentPlayer());
-		assertEquals(players.get(2), board.getNextPlayer());
-		assertEquals(players.get(1), board.getPreviousPlayer());
+		board.changeRotateDirectionAndResetPositions(Direction.COUNTERCLOCKWISE);
+		comparePlayerPositions(players, board, 0, 2, 1);
 	}
-	
+
 	@Test
 	public void testChangeRotateDirectionAndResetPositionsMax() {
 		ArrayList<Player> players = new ArrayList<Player>();
@@ -113,19 +107,14 @@ public class GameBoardTests {
 		players.add(new Player("Thor"));
 
 		GameBoard board = new GameBoard(players);
-		board.changeRotateDirectionAndResetPositions(Direction.CLOCKWISE);
-
-		assertEquals(players.get(0), board.getCurrentPlayer());
-		assertEquals(players.get(1), board.getNextPlayer());
-		assertEquals(players.get(6), board.getPreviousPlayer());
 		
-		board.changeRotateDirectionAndResetPositions(Direction.COUNTERCLOCKWISE);
+		board.changeRotateDirectionAndResetPositions(Direction.CLOCKWISE);
+		comparePlayerPositions(players, board, 0, 1, 6);
 
-		assertEquals(players.get(0), board.getCurrentPlayer());
-		assertEquals(players.get(6), board.getNextPlayer());
-		assertEquals(players.get(1), board.getPreviousPlayer());
+		board.changeRotateDirectionAndResetPositions(Direction.COUNTERCLOCKWISE);
+		comparePlayerPositions(players, board, 0, 6, 1);
 	}
-	
+
 	@Test
 	public void testChangeRotateDirectionAndResetPositionsAfterOppositeRotate() {
 		ArrayList<Player> players = new ArrayList<Player>();
@@ -138,20 +127,15 @@ public class GameBoardTests {
 		GameBoard board = new GameBoard(players);
 		board.rotateClockwise();
 		board.changeRotateDirectionAndResetPositions(Direction.COUNTERCLOCKWISE);
+		
+		comparePlayerPositions(players, board, 0, 4, 1);
 
-		assertEquals(players.get(0), board.getCurrentPlayer());
-		assertEquals(players.get(4), board.getNextPlayer());
-		assertEquals(players.get(1), board.getPreviousPlayer());
-		
-		
 		board.rotateCounterClockwise();
 		board.changeRotateDirectionAndResetPositions(Direction.CLOCKWISE);
-
-		assertEquals(players.get(0), board.getCurrentPlayer());
-		assertEquals(players.get(1), board.getNextPlayer());
-		assertEquals(players.get(4), board.getPreviousPlayer());
+		
+		comparePlayerPositions(players, board, 0, 1, 4);
 	}
-	
+
 	@Test
 	public void testGetPlayerPositionsOnStartMin() {
 		ArrayList<Player> players = new ArrayList<Player>();
@@ -160,13 +144,10 @@ public class GameBoardTests {
 		players.add(new Player("Black Widow"));
 
 		GameBoard board = new GameBoard(players);
-
-		assertEquals(players.get(0), board.getCurrentPlayer());
-		assertEquals(players.get(1), board.getNextPlayer());
-		assertEquals(players.get(2), board.getPreviousPlayer());
+		
+		comparePlayerPositions(players, board, 0, 1, 2);
 	}
 
-	
 	@Test
 	public void testGetPlayerPositionsOnStartMax() {
 		ArrayList<Player> players = new ArrayList<Player>();
@@ -179,12 +160,10 @@ public class GameBoardTests {
 		players.add(new Player("Thor"));
 
 		GameBoard board = new GameBoard(players);
-
-		assertEquals(players.get(0), board.getCurrentPlayer());
-		assertEquals(players.get(1), board.getNextPlayer());
-		assertEquals(players.get(6), board.getPreviousPlayer());
+		
+		comparePlayerPositions(players, board, 0, 1, 6);
 	}
-	
+
 	@Test
 	public void testGetPlayerPositionsOnRotateClockwiseMin() {
 		ArrayList<Player> players = new ArrayList<Player>();
@@ -194,12 +173,10 @@ public class GameBoardTests {
 
 		GameBoard board = new GameBoard(players);
 		board.rotateClockwise();
-
-		assertEquals(players.get(1), board.getCurrentPlayer());
-		assertEquals(players.get(2), board.getNextPlayer());
-		assertEquals(players.get(0), board.getPreviousPlayer());
+		
+		comparePlayerPositions(players, board, 1, 2, 0);
 	}
-	
+
 	@Test
 	public void testGetPlayerPositionsOnRotateClockwiseMax() {
 		ArrayList<Player> players = new ArrayList<Player>();
@@ -213,12 +190,10 @@ public class GameBoardTests {
 
 		GameBoard board = new GameBoard(players);
 		board.rotateClockwise();
-
-		assertEquals(players.get(1), board.getCurrentPlayer());
-		assertEquals(players.get(2), board.getNextPlayer());
-		assertEquals(players.get(0), board.getPreviousPlayer());
+		
+		comparePlayerPositions(players, board, 1, 2, 0);
 	}
-	
+
 	@Test
 	public void testGetPlayerPositionsOnRotateClockwiseTwice() {
 		ArrayList<Player> players = new ArrayList<Player>();
@@ -232,11 +207,9 @@ public class GameBoardTests {
 		board.rotateClockwise();
 		board.rotateClockwise();
 
-		assertEquals(players.get(2), board.getCurrentPlayer());
-		assertEquals(players.get(3), board.getNextPlayer());
-		assertEquals(players.get(1), board.getPreviousPlayer());
+		comparePlayerPositions(players, board, 2, 3, 1);
 	}
-	
+
 	@Test
 	public void testGetPlayerPositionsOnRotateClockwiseMany() {
 		ArrayList<Player> players = new ArrayList<Player>();
@@ -248,15 +221,13 @@ public class GameBoardTests {
 
 		GameBoard board = new GameBoard(players);
 
-		for(int i = 0; i < 10; i++){
+		for (int i = 0; i < 10; i++) {
 			board.rotateClockwise();
 		}
 		
-		assertEquals(players.get(0), board.getCurrentPlayer());
-		assertEquals(players.get(1), board.getNextPlayer());
-		assertEquals(players.get(4), board.getPreviousPlayer());
+		comparePlayerPositions(players, board, 0, 1, 4);
 	}
-	
+
 	@Test
 	public void testGetPlayerPositionsOnRotateCounterClockwiseMin() {
 		ArrayList<Player> players = new ArrayList<Player>();
@@ -267,12 +238,10 @@ public class GameBoardTests {
 		GameBoard board = new GameBoard(players);
 		board.changeRotateDirectionAndResetPositions(Direction.COUNTERCLOCKWISE);
 		board.rotateCounterClockwise();
-
-		assertEquals(players.get(2), board.getCurrentPlayer());
-		assertEquals(players.get(1), board.getNextPlayer());
-		assertEquals(players.get(0), board.getPreviousPlayer());
+		
+		comparePlayerPositions(players, board, 2, 1, 0);
 	}
-	
+
 	@Test
 	public void testGetPlayerPositionsOnRotateCounterClockwiseMax() {
 		ArrayList<Player> players = new ArrayList<Player>();
@@ -283,16 +252,14 @@ public class GameBoardTests {
 		players.add(new Player("Iron Man"));
 		players.add(new Player("Spider Man"));
 		players.add(new Player("Thor"));
-		
+
 		GameBoard board = new GameBoard(players);
 		board.changeRotateDirectionAndResetPositions(Direction.COUNTERCLOCKWISE);
 		board.rotateCounterClockwise();
 
-		assertEquals(players.get(6), board.getCurrentPlayer());
-		assertEquals(players.get(5), board.getNextPlayer());
-		assertEquals(players.get(0), board.getPreviousPlayer());
+		comparePlayerPositions(players, board, 6, 5, 0);
 	}
-	
+
 	@Test
 	public void testGetPlayerPositionsOnRotateCounterClockwiseTwice() {
 		ArrayList<Player> players = new ArrayList<Player>();
@@ -307,11 +274,9 @@ public class GameBoardTests {
 		board.rotateCounterClockwise();
 		board.rotateCounterClockwise();
 
-		assertEquals(players.get(3), board.getCurrentPlayer());
-		assertEquals(players.get(2), board.getNextPlayer());
-		assertEquals(players.get(4), board.getPreviousPlayer());
+		comparePlayerPositions(players, board, 3, 2, 4);
 	}
-	
+
 	@Test
 	public void testGetPlayerPositionsOnRotateCounterClockwiseMany() {
 		ArrayList<Player> players = new ArrayList<Player>();
@@ -324,12 +289,17 @@ public class GameBoardTests {
 		GameBoard board = new GameBoard(players);
 		board.changeRotateDirectionAndResetPositions(Direction.COUNTERCLOCKWISE);
 
-		for(int i = 0; i < 10; i++){
+		for (int i = 0; i < 10; i++) {
 			board.rotateCounterClockwise();
 		}
-		
-		assertEquals(players.get(0), board.getCurrentPlayer());
-		assertEquals(players.get(4), board.getNextPlayer());
-		assertEquals(players.get(1), board.getPreviousPlayer());
+
+		comparePlayerPositions(players, board, 0, 4, 1);
+	}
+
+	public void comparePlayerPositions(ArrayList<Player> players, GameBoard board, int currIndex, int nextIndex,
+			int previousIndex) {
+		assertEquals(players.get(currIndex), board.getCurrentPlayer());
+		assertEquals(players.get(nextIndex), board.getNextPlayer());
+		assertEquals(players.get(previousIndex), board.getPreviousPlayer());
 	}
 }
