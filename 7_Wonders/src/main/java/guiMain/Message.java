08c2ad4d001f6ae22ca909071bff7a55f0240608
Shijun Yu <y.s.j@live.cn>
2017-04-06 20:17:44 -0400

    JOptionPane for Pop-up Messages

--- 7_Wonders/src/main/java/guiMain/Message.java
+++ 7_Wonders/src/main/java/guiMain/Message.java
@@ -2,24 +2,15 @@ package guiMain;
 
 import java.awt.Graphics;
 
+import javax.swing.JOptionPane;
+
 import GuiDataStructures.Constants;
 
 public class Message {
 	private String message = "";
 	
-
-	
 	public void setMessage(String message) {
 		this.message = message;
-	}
-	
-	public void drawMessageOnScreen(Graphics graphics) {
-		graphics.setFont(Constants.ErrorFont);
-		graphics.setColor(Constants.TitleColor);
-		graphics.drawString(this.message, Constants.ErrorMessagePosition.x, Constants.ErrorMessagePosition.y);
-	}
-	
-	public void clearMessage() {
-		this.message = "";
+		JOptionPane.showMessageDialog(null, this.message);
 	}
 }
