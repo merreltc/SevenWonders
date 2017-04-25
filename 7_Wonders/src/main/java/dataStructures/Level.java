5d415cece5c8c5587becd9270a50b1b05d5b0d14
Trinity Merrell <merrelt@rose-hulman.edu>
2017-04-25 16:40:14 -0400

    TDD: testPriority2Level passes

--- 7_Wonders/src/main/java/dataStructures/Level.java
+++ 7_Wonders/src/main/java/dataStructures/Level.java
@@ -1,13 +1,15 @@
 package dataStructures;
 
 public class Level {
+	private int priority;
 
 	public Level(int priority, Cost cost, Effect effect) {
+		this.priority = priority;
 		
 	}
 
 	public int getPriority() {
-		return 1;
+		return this.priority;
 	}
 
 }
--- 7_Wonders/src/test/java/dataStructureTests/LevelTest.java
+++ 7_Wonders/src/test/java/dataStructureTests/LevelTest.java
@@ -12,13 +12,23 @@ import dataStructures.Level;
 public class LevelTest {
 
 	@Test
-	public void testBasicLevel() {
+	public void testPriority1Level() {
 		Cost cost = EasyMock.createStrictMock(Cost.class);
 		Effect effect = EasyMock.createStrictMock(Effect.class);
 		
 		int priority = 1;
 		Level level = new Level(priority, cost, effect);
 		assertEquals(1, level.getPriority());
+	}
+	
+	@Test
+	public void testPriority2Level() {
+		Cost cost = EasyMock.createStrictMock(Cost.class);
+		Effect effect = EasyMock.createStrictMock(Effect.class);
+		
+		int priority = 2;
+		Level level = new Level(priority, cost, effect);
+		assertEquals(2, level.getPriority());
 	}
 
 }
