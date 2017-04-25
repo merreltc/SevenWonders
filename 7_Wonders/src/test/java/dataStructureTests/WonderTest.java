6fe828bdbddf7adb9293138dd1c6efc6e2018464
Trinity Merrell <merrelt@rose-hulman.edu>
2017-04-25 16:29:57 -0400

    TDD: testNumLevelsSideA passes

---
+++ 7_Wonders/src/test/java/dataStructureTests/WonderTest.java
@@ -0,0 +1,18 @@
+package dataStructureTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import dataStructures.Wonder;
+
+public class WonderTest {
+
+	@Test
+	public void testNumLevelsSideA() {
+		Wonder wonder = new Wonder('A');
+		
+		assertEquals(3, wonder.getNumLevels());
+	}
+
+}
