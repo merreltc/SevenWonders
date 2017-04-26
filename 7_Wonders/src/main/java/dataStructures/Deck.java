085cfd558ea53177bf6a4875dd1a0388f012427d
Carrie Utter <uttercm@rose-hulman.edu>
2017-04-25 21:25:00 -0400

    TDD: testDeckConstructor passes

---
+++ 7_Wonders/src/main/java/dataStructures/Deck.java
@@ -0,0 +1,23 @@
+package dataStructures;
+
+import java.util.ArrayList;
+
+public class Deck {
+	private ArrayList<Card> cards = new ArrayList<Card>();
+	
+	public enum Age {
+		AGE1
+	}
+
+	public Deck(Age age, ArrayList<Card> cards) {
+		this.cards = cards;
+	}
+
+	public Age getAge() {
+		return Age.AGE1;
+	}
+
+	public ArrayList<Card> getCards() {
+		return this.cards;
+	}
+}
--- 7_Wonders/src/test/java/dataStructureTests/DeckTest.java
+++ 7_Wonders/src/test/java/dataStructureTests/DeckTest.java
@@ -1,180 +1,54 @@
 package dataStructureTests;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
 
+import org.easymock.EasyMock;
 import org.junit.Test;
 
 import dataStructures.Card;
 import dataStructures.Cost;
-import dataStructures.Cost.CostType;
+import dataStructures.Deck;
 import dataStructures.Effect;
-import dataStructures.Effect.Direction;
-import dataStructures.Effect.EffectType;
-import dataStructures.EntityEffect;
-import dataStructures.EntityEffect.EntityType;
-import dataStructures.EntityEffect.Good;
-import dataStructures.EntityEffect.Resource;
-import dataStructures.EntityEffect.Science;
-import dataStructures.ValueEffect;
 import dataStructures.Card.CardType;
-import dataStructures.ValueEffect.AffectingEntity;
-import dataStructures.ValueEffect.Value;
-import json.JSONArray;
-import json.JSONException;
-import json.JSONObject;
+import dataStructures.Deck.Age;
 
 public class DeckTest {
 
 	@Test
-	public void testCreateAge1Deck3Players() {
-		int numPlayers = 3;
-		Deck deck = new Deck("age1", numPlayers);
-		assertEquals(49, deck.getNumCards());
+	public void testDeckConstructor() {
+		ArrayList<Card> cards = new ArrayList<Card>();
+		initiateCardes(cards);
+		
+		Deck deck = new Deck(Age.AGE1, cards);
+		
+		assertEquals(Age.AGE1, deck.getAge());
+		assertEquals(cards, deck.getCards());
+	}
 
-		String jsonData = readFile("age1cards.json");
-		JSONObject jsonObj = new JSONObject(jsonData);
-		JSONArray jarr = new JSONArray(jsonObj.getJSONArray("age1"));
+	private void initiateCardes(ArrayList<Card> cards) {
+		ArrayList<Integer> frequency = new ArrayList<Integer>();
+		frequency.add(3);
+		frequency.add(6);
+		frequency.add(7);
 
-		for (int i = 0; i < jarr.length(); i++) {
-			JSONObject cardData = jarr.getJSONObject(i);
-			String cardName = cardData.getString("cardName");
+		Cost cost = EasyMock.createStrictMock(Cost.class);
+		Effect effect = EasyMock.createStrictMock(Effect.class);
 
-			JSONArray frequencyByNumPlayersJSON = cardData.getJSONArray("frequencyByNumPlayers");
+		Card forumCard = new Card("Forum", frequency, CardType.RAWMATERIAL, cost, effect, "Trading Post", "None");
+		
+		ArrayList<Integer> frequencyS = new ArrayList<Integer>();
+		frequency.add(3);
+		frequency.add(7);
 
-			ArrayList<Integer> frequencyByNumPlayers = new ArrayList<Integer>();
-			for (int j = 0; j < frequencyByNumPlayersJSON.length(); j++) {
-				frequencyByNumPlayers.add(frequencyByNumPlayersJSON.getInt(j));
-			}
-			
-			CardType cardType = CardType.valueOf(cardData.getString("cardType"));
+		Cost costS = EasyMock.createStrictMock(Cost.class);
+		Effect effectS = EasyMock.createStrictMock(Effect.class);
 
-			JSONObject costObj = cardData.getJSONObject("Cost");
-			String costTypeS = costObj.getString("CostType");
-			CostType costType;
-			Cost cost;
-
-			switch (costTypeS) {
-			case "COIN":
-				costType = CostType.COIN;
-				int coinCost = costObj.getInt("coinCost");
-				cost = new Cost(costType, coinCost);
-				break;
-			case "NONE":
-				costType = CostType.NONE;
-				cost = new Cost(costType, 0);
-				break;
-			case "VALUE":
-				costType = CostType.RESOURCE;
-				JSONArray costs = costObj.getJSONArray("cost");
-
-				HashMap<Enum, Integer> resourceCosts = new HashMap<Enum, Integer>();
-
-				for (int c = 0; c < costs.length(); c++) {
-					JSONObject resource = costs.getJSONObject(c);
-					String resourceType = resource.getString("RESOURCE");
-					int amount = resource.getInt("amount");
-					Resource costResource = Resource.valueOf(resourceType);
-
-					resourceCosts.put(costResource, amount);
-				}
-
-				cost = new Cost(costType, resourceCosts);
-				break;
-			default:
-				costType = CostType.NONE;
-				cost = new Cost(costType, 0);
-				break;
-			}
-
-			JSONObject effectObj = cardData.getJSONObject("Effect");
-			String effectStr = costObj.getString("EffectType");
-			EffectType effectTypeEnum;
-			Effect effect;
-
-			switch (effectStr) {
-			case "ENTITY":
-				effectTypeEnum = EffectType.ENTITY;
-				String entityStr = effectObj.getString("EntityType");
-				EntityType entityEnum = EntityType.valueOf(entityStr);
-				HashMap<Enum, Integer> entitiesAndAmounts = new HashMap<Enum, Integer>();
-
-				JSONArray entitiesJSON = effectObj.getJSONArray("entitiesAndAmounts");
-
-				for (int entity = 0; entity < entitiesJSON.length(); entity++) {
-					JSONObject entityToAdd = entitiesJSON.getJSONObject(entity);
-
-					Set<String> entityKeys = entityToAdd.keySet();
-
-					String entityType = entityKeys.iterator().next();
-					int entityAmount;
-
-					switch (entityType) {
-					case "Science":
-						entityAmount = entityToAdd.getInt("entityAmount");
-						Science science = Science.valueOf(entityToAdd.getString(entityType));
-						entitiesAndAmounts.put(science, entityAmount);
-						break;
-					case "Good":
-						entityAmount = entityToAdd.getInt("entityAmount");
-						Good good = Good.valueOf(entityToAdd.getString(entityType));
-						entitiesAndAmounts.put(good, entityAmount);
-					case "Resource":
-						entityAmount = entityToAdd.getInt("entityAmount");
-						Resource resource = Resource.valueOf(entityToAdd.getString(entityType));
-						entitiesAndAmounts.put(resource, entityAmount);
-						break;
-					default:
-						break;
-					}
-				}
-
-				effect = new EntityEffect(effectTypeEnum, entityEnum, entitiesAndAmounts);
-				break;
-			case "VALUE":
-				effectTypeEnum = EffectType.VALUE;
-
-				try {
-					String affecting = effectObj.getString("AffectingEntities");
-					Value value = Value.valueOf(effectObj.getString("Value"));
-					AffectingEntity affectingEntities = AffectingEntity.valueOf(affecting);
-					int valueAmount = effectObj.getInt("valueAmount");
-					
-					if (affecting.equals("NONE")){
-						effect = new ValueEffect(effectTypeEnum, value, affectingEntities, valueAmount);
-						break;
-					}
-					
-					Direction direction = Direction.valueOf(effectObj.getString("Direction"));
-					effect = new ValueEffect(effectTypeEnum, value, affectingEntities, direction, valueAmount);
-					break;
-				} catch (JSONException exception) { //the affecting entities was an array
-					JSONArray affecting = effectObj.getJSONArray("AffectingEntities");
-					Value value = Value.valueOf(effectObj.getString("Value"));
-					HashMap<Enum, Integer> affectingEntities = new HashMap<Enum, Integer>();
-					
-					for (int ae = 0; ae < affecting.length(); ae++){
-						affectingEntities.put(AffectingEntity.valueOf(affecting.getString(ae)), 1);
-					}
-
-					effect = new ValueEffect(effectTypeEnum, value, affectingEntities);
-				}
-
-				break;
-			}
-			
-			Card card;
-			
-			if(cardType.equals(CardType.GUILD)){
-				card = new Card(cardName, cardType, cost, effect);
-			}else{
-				card = new Card(cardName, frequencyByNumPlayers, cardType, cost, effect);
-			}
-		}
+		Card statueCard = new Card("Statue", frequencyS, CardType.RAWMATERIAL, costS, effectS, "Theater", "Gardens");
+		
+		cards.add(forumCard);
+		cards.add(statueCard);
 	}
 
 }
