e1ab816c807186652ec16baff94389600ffb8adb
EricMoorman <moormaet@rose-hulman.edu>
2017-05-16 19:24:05 -0400

    passes activateWorkersGuildEffect

--- 7_Wonders/src/main/java/backend/handlers/EndGameHandler.java
+++ 7_Wonders/src/main/java/backend/handlers/EndGameHandler.java
@@ -1,7 +1,82 @@
 package backend.handlers;
 
+import java.util.ArrayList;
+
+import dataStructures.gameMaterials.Card;
+import dataStructures.gameMaterials.Card.CardType;
+import dataStructures.gameMaterials.Effect;
+import dataStructures.gameMaterials.Effect.Direction;
+import dataStructures.gameMaterials.ValueEffect.AffectingEntity;
+import dataStructures.gameMaterials.ValueEffect;
+import dataStructures.playerData.Player;
+
 public class EndGameHandler {
 
+	public int getPointsFromGuildCards(Player current, Player left, Player right) {
+		int points = 0;
+		
+		int counter = 0;
+		for (;;){
+			try{
+				Card card = current.getCardFromEndGame(counter);
+				
+				if (card.getEffect().getDirection() == Direction.SELF || card.getEffect().getDirection() == Direction.ALL){
+					points += this.useEffect(card, current);
+				}
+				if (card.getEffect().getDirection() == Direction.LEFT || card.getEffect().getDirection() == Direction.ALL
+						|| card.getEffect().getDirection() == Direction.NEIGHBORS){
+					points += this.useEffect(card, left);
+				}
+				if (card.getEffect().getDirection() == Direction.RIGHT || card.getEffect().getDirection() == Direction.ALL
+						|| card.getEffect().getDirection() == Direction.NEIGHBORS){
+					points += this.useEffect(card, right);
+				}
+				
+				counter++;
+			
+			}catch(Exception e){
+				break;
+			}
+		}
+		
+		return points;
+	}
+
+	private int useEffect(Card card, Player player){
+		int total = 0;
+		ValueEffect effect = (ValueEffect) card.getEffect();
+		if (effect.getAffectingEntity() != AffectingEntity.NONE){
+			total += runEffectOnPlayer(effect,effect.getAffectingEntity(),player);
+		}else {
+			for (Enum currentType : effect.getAffectingEntities().keySet()){
+				total += runEffectOnPlayer(effect, (AffectingEntity) currentType,player);
+			}
+		}
+
+		return total;
+	}
 	
+	private int runEffectOnPlayer(ValueEffect effect, AffectingEntity currentType, Player players){
+		int total = 0;
+		int valueToAdd = 0;
+		if (!effect.getAffectingEntities().containsKey(currentType)){
+			valueToAdd = effect.getValueAmount();
+		}else{
+			valueToAdd = effect.getAffectingEntities().get(currentType);;
+		}
+		
+		ArrayList<Card> cards = players.getStoragePile();
+		for (Card currentCard : cards){
+			System.out.println(currentType);
+			System.out.println(currentCard.getCardType());
+			if (currentType.toString().contains(currentCard.getCardType().toString())){
+				total += valueToAdd;
+			}else if (currentCard.getCardType() == CardType.RAWMATERIAL && currentType == AffectingEntity.RAWRESOURCES){
+				System.out.println(valueToAdd);
+				total += valueToAdd;
+			}
+		}
+		return total;
+	}
 	
 }
--- 7_Wonders/src/test/java/backendTests/EndGameHandlerTest.java
+++ 7_Wonders/src/test/java/backendTests/EndGameHandlerTest.java
@@ -9,44 +9,98 @@ import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
 
+import backend.handlers.EndGameHandler;
 import constants.GeneralEnums.CostType;
 import constants.GeneralEnums.Good;
 import constants.GeneralEnums.RawResource;
+import constants.GeneralEnums.Science;
 import dataStructures.gameMaterials.Card;
 import dataStructures.gameMaterials.Cost;
 import dataStructures.gameMaterials.Effect;
+import dataStructures.gameMaterials.EntityEffect;
 import dataStructures.gameMaterials.ValueEffect;
 import dataStructures.gameMaterials.Card.CardType;
 import dataStructures.gameMaterials.Effect.Direction;
 import dataStructures.gameMaterials.Effect.EffectType;
+import dataStructures.gameMaterials.EntityEffect.EntityType;
 import dataStructures.gameMaterials.ValueEffect.AffectingEntity;
 import dataStructures.gameMaterials.ValueEffect.Value;
 import dataStructures.playerData.Player;
+import junit.framework.Assert;
 
 public class EndGameHandlerTest {
 	
-	Player player1;
-	Player player2;
-	Player player3;
-	Player player4;
-	Player player5;
-	Player player6;
-	Player player7;
-	
-	@Before
 	public void initializePlayers(){
-		player1 = EasyMock.mock(Player.class);
+		Player player1 = EasyMock.mock(Player.class);
 	    EasyMock.expect(player1.getConflictTotal()).andReturn(3);
 	    EasyMock.expect(player1.getCoinTotal()).andReturn(9);
+	    EasyMock.expect(player1.getNumVictoryPoints()).andReturn(4);
 	    EasyMock.expect(player1.getCardFromEndGame(0)).andReturn(this.createCraftsmenGuild());
-	    EasyMock.expect(player1);
+	    EasyMock.expect(player1.getNumberOfEachScience()).andReturn(new int[]{1,1,1});
+	    
+	    Player player2 = EasyMock.mock(Player.class);
+	    EasyMock.expect(player2.getConflictTotal()).andReturn(4);
+	    EasyMock.expect(player2.getCoinTotal()).andReturn(6);
+	    EasyMock.expect(player2.getNumVictoryPoints()).andReturn(2);
+	    EasyMock.expect(player2.getCardFromEndGame(0)).andReturn(this.createCraftsmenGuild());
+	    EasyMock.expect(player2.getNumberOfEachScience()).andReturn(new int[]{3,1,0});
+	    
+	    Player player3 = EasyMock.mock(Player.class);
+	    EasyMock.expect(player3.getConflictTotal()).andReturn(7);
+	    EasyMock.expect(player3.getCoinTotal()).andReturn(2);
+	    EasyMock.expect(player3.getNumVictoryPoints()).andReturn(2);
+	    EasyMock.expect(player3.getCardFromEndGame(0)).andReturn(this.createWorkersGuild());
+	    EasyMock.expect(player3.getNumberOfEachScience()).andReturn(new int[]{1,2,2});
+	    
+	    Player player4 = EasyMock.mock(Player.class);
+	    EasyMock.expect(player4.getConflictTotal()).andReturn(-3);
+	    EasyMock.expect(player4.getCoinTotal()).andReturn(1);
+	    EasyMock.expect(player4.getNumVictoryPoints()).andReturn(5);
+	    EasyMock.expect(player4.getCardFromEndGame(0)).andReturn(this.createCraftsmenGuild());
+	    EasyMock.expect(player4.getNumberOfEachScience()).andReturn(new int[]{5,1,2});
+	    
+	    Player player5 = EasyMock.mock(Player.class);
+	    EasyMock.expect(player5.getConflictTotal()).andReturn(5);
+	    EasyMock.expect(player5.getCoinTotal()).andReturn(6);
+	    EasyMock.expect(player5.getNumVictoryPoints()).andReturn(1);
+	    EasyMock.expect(player5.getCardFromEndGame(0)).andReturn(this.createCraftsmenGuild());
+	    EasyMock.expect(player5.getNumberOfEachScience()).andReturn(new int[]{1,0,1});
+	    
+	    Player player6 = EasyMock.mock(Player.class);
+	    EasyMock.expect(player6.getConflictTotal()).andReturn(6);
+	    EasyMock.expect(player6.getCoinTotal()).andReturn(9);
+	    EasyMock.expect(player6.getNumVictoryPoints()).andReturn(0);
+	    EasyMock.expect(player6.getCardFromEndGame(0)).andReturn(this.createWorkersGuild());
+	    EasyMock.expect(player6.getNumberOfEachScience()).andReturn(new int[]{1,5,2});
+	    
+	    Player player7 = EasyMock.mock(Player.class);
+	    EasyMock.expect(player7.getConflictTotal()).andReturn(4);
+	    EasyMock.expect(player7.getCoinTotal()).andReturn(1);
+	    EasyMock.expect(player7.getNumVictoryPoints()).andReturn(0);
+	    EasyMock.expect(player7.getCardFromEndGame(0)).andReturn(this.createCraftsmenGuild());
+	    EasyMock.expect(player7.getNumberOfEachScience()).andReturn(new int[]{1,1,1});
 	}
 
 	@Test
-	public void GetWinnerTwo(){
-		ArrayList<Player> players = new ArrayList<Player>();
-		Player winner = EasyMock.mock(Player.class);
+	public void activateWorkersGuildEffect(){
+		ArrayList<Card> cards = new ArrayList<Card>();
+		cards.add(createRawMaterialCard());
+		cards.add(createRawMaterialCard());
 		
+		Player player1 = EasyMock.mock(Player.class);
+		Player player2 = EasyMock.mock(Player.class);
+		Player player3 = EasyMock.mock(Player.class);
+		
+		EasyMock.expect(player2.getCardFromEndGame(0)).andReturn(this.createWorkersGuild());
+		EasyMock.expect(player2.getCardFromEndGame(1)).andThrow(new IllegalArgumentException(""));
+		
+		EndGameHandler end = new EndGameHandler();
+		EasyMock.expect(player1.getStoragePile()).andReturn(cards);
+		EasyMock.expect(player3.getStoragePile()).andReturn(cards);
+		
+		EasyMock.replay(player1,player2,player3);
+		
+		Assert.assertEquals(4, end.getPointsFromGuildCards(player2, player1, player3));
 	}
 	
 	private Card createWorkersGuild(){
@@ -72,7 +126,7 @@ public class EndGameHandlerTest {
 		return card;
 	}
 	
-	private Card createCourthouseCard(){
+	private Card createCivilianCard(){
 		HashMap<Enum, Integer> costs = new HashMap<Enum, Integer>();
 		costs.put(Good.LOOM, 1);
 		costs.put(RawResource.CLAY, 2);
@@ -81,5 +135,55 @@ public class EndGameHandlerTest {
 		Card card = new Card("Courthouse", CardType.SCIENTIFICSTRUCTURE, cost, effect);
 		return card;
 	}
+	
+	private Card createRawMaterialCard(){
+		Cost cost = new Cost(CostType.NONE, 0);
+		HashMap<Enum, Integer> entitiesAndAmounts = new HashMap<Enum, Integer>();
+		entitiesAndAmounts.put(RawResource.LUMBER, 1);
+		Effect effect = new EntityEffect(EffectType.ENTITY, EntityType.RESOURCE, entitiesAndAmounts);
+
+		Card card = new Card("Lumber Yard", CardType.RAWMATERIAL, cost, effect);
+		return card;
+	}
+	
+	private Card createManufacturedCard(){
+		HashMap<Enum, Integer> entitiesAndAmounts = new HashMap<Enum, Integer>();
+		Cost cost = new Cost(CostType.NONE, 0);
+		entitiesAndAmounts.put(Good.LOOM, 1);
+		Effect effect = new EntityEffect(EffectType.ENTITY, EntityType.MANUFACTUREDGOOD, entitiesAndAmounts);
+
+		Card card = new Card("Loom", CardType.MANUFACTUREDGOOD, cost, effect);
+		return card;
+	}
+	
+	private Card createCommercialCard(){
+		Cost cost = new Cost(CostType.NONE, 0);
+		Effect effect = new ValueEffect(EffectType.VALUE, Value.COMMERCE, AffectingEntity.NONE, 5);
+		Card card = new Card("Tavern", CardType.COMMERCIALSTRUCTURE, cost, effect);
+		return card;
+	}
+	
+	private Card createMilitaryCard(){
+		HashMap<Enum, Integer> costs = new HashMap<Enum, Integer>();
+		costs.put(RawResource.LUMBER, 1);
+		costs.put(RawResource.ORE, 1);
+		costs.put(RawResource.CLAY, 1);
+		Cost cost = new Cost(CostType.RESOURCE, costs);
+		Effect effect = new ValueEffect(EffectType.VALUE, Value.MILITARY, AffectingEntity.NONE, 2);
+		Card card = new Card("Stables", CardType.MILITARYSTRUCTURE, cost, effect);
+		return card;
+	}
+	
+	private Card createWorkshop(){
+		HashMap<Enum, Integer>costs = new HashMap<Enum, Integer>();
+		costs.put(Good.GLASS, 1);
+		Cost cost = new Cost(CostType.GOOD, costs);
+		HashMap<Enum, Integer> entitiesAndAmounts = new HashMap<Enum, Integer>();
+		entitiesAndAmounts.put(Science.WHEEL, 1);
+		Effect effect = new EntityEffect(EffectType.ENTITY, EntityType.SCIENCE, entitiesAndAmounts);
+		Card card = new Card("Workshop", CardType.SCIENTIFICSTRUCTURE, cost, effect);
+		return card;
+	}
+	
 
 }
