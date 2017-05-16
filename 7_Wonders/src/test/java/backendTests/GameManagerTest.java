5dcf09defaad3592b1b4e21cc8a474dd6e62398e
Carrie Utter <uttercm@rose-hulman.edu>
2017-05-16 16:44:41 -0400

    TDD: testValidTradeForDiscountEastTradingPostCounter passes

--- 7_Wonders/src/test/java/backendTests/GameManagerTest.java
+++ 7_Wonders/src/test/java/backendTests/GameManagerTest.java
@@ -428,6 +428,28 @@ public class GameManagerTest {
 	}
 	
 	@Test
+	public void testValidTradeForDiscountEastTradingPostCounter() {
+		ArrayList<String> playerNames = new ArrayList<String>(
+				Arrays.asList("Wolverine", "Captain America", "Black Widow"));
+
+		GameManager manager = new GameManager(playerNames, new SetUpPlayerHandler(),
+				new SetUpDeckHandler(), new TurnHandler(), new PlayerTurnHandler());
+		manager.changeRotateDirectionAndResetPositions(Rotation.COUNTERCLOCKWISE);
+		Deck deck = manager.getGameBoard().getDeck();
+		Player current = manager.getCurrentPlayer();
+		Player right = manager.getPreviousPlayer();
+		ArrayList<Card> storage = new ArrayList<Card>();
+		storage.add(deck.getCard(12)); // east trading post
+		current.setStoragePile(storage);
+		ArrayList<Card> storage2 = new ArrayList<Card>();
+		storage2.add(deck.getCard(0)); // lumber
+		right.setStoragePile(storage2);
+		manager.tradeForEntity(current, right, RawResource.LUMBER);
+		assertTrue(current.getCurrentTrades().containsKey(RawResource.LUMBER));
+		assertEquals(4, right.getCoinTotal());
+	}
+	
+	@Test
 	public void testValidTradeForDiscountWestTradingPost() {
 		ArrayList<String> playerNames = new ArrayList<String>(
 				Arrays.asList("Wolverine", "Captain America", "Black Widow"));
