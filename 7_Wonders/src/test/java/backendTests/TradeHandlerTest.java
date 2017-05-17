bd9b292eae844b58acca638840a3ddc2b559f4a3
merreltc <merreltc@rose-hulman.edu>
2017-05-17 01:12:26 -0400

    TDD: refactored TradeHandlerTest. all tests pass
    adding GeneralEnums. previous Wonder tests will not pass until this
    commit

--- 7_Wonders/src/test/java/backendTests/TradeHandlerTest.java
+++ 7_Wonders/src/test/java/backendTests/TradeHandlerTest.java
@@ -24,35 +24,31 @@ import exceptions.InsufficientFundsException;
 import exceptions.InvalidTradeException;
 
 public class TradeHandlerTest {
-	private Player player1, player2, player3, player4, player5;
+	private Player player1, player2;
 	private Wonder wonder;
+	private Deck testDeck;
 
 	@Before
 	public void setUp() {
 		this.wonder = EasyMock.createStrictMock(Wonder.class);
 		this.player1 = new Player("Jane Doe", this.wonder);
 		this.player2 = new Player("Jane Doe", this.wonder);
-		this.player3 = new Player("Jane Doe", this.wonder);
-		this.player4 = new Player("Jane Doe", this.wonder);
-		this.player5 = new Player("Jane Doe", this.wonder);
+		this.testDeck = EasyMock.partialMockBuilder(Deck.class).createMock();
 	}
 
 	@Test
 	public void testSingleTradeValue1Coins() {
 		TradeHandler.tradeFromToValue1(this.player1, this.player2, 1);
-
 		assertEquals(4, this.player2.getCoinTotal());
 		assertEquals(4, this.player2.getNumValue1Coins());
 		assertEquals(2, this.player1.getCoinTotal());
 		assertEquals(2, this.player1.getNumValue1Coins());
-
 	}
 
 	@Test
 	public void testMultiTradesValue1Coins() {
 		TradeHandler.tradeFromToValue1(this.player1, this.player2, 2);
 		TradeHandler.tradeFromToValue1(this.player1, this.player2, 1);
-
 		assertEquals(6, this.player2.getCoinTotal());
 		assertEquals(6, this.player2.getNumValue1Coins());
 		assertEquals(0, this.player1.getCoinTotal());
@@ -70,7 +66,6 @@ public class TradeHandlerTest {
 	public void testSingleTradeValue3Coins() {
 		this.player1.addValue3(1, Chip.ChipType.COIN);
 		TradeHandler.tradeFromToValue3(this.player1, this.player2, 1);
-
 		assertEquals(6, this.player2.getCoinTotal());
 		assertEquals(1, this.player2.getNumValue3Coins());
 		assertEquals(3, this.player1.getCoinTotal());
@@ -82,7 +77,6 @@ public class TradeHandlerTest {
 		this.player1.addValue3(3, Chip.ChipType.COIN);
 		TradeHandler.tradeFromToValue3(this.player1, this.player2, 2);
 		TradeHandler.tradeFromToValue3(this.player1, this.player2, 1);
-
 		assertEquals(12, this.player2.getCoinTotal());
 		assertEquals(3, this.player2.getNumValue3Coins());
 		assertEquals(3, this.player1.getCoinTotal());
@@ -98,7 +92,6 @@ public class TradeHandlerTest {
 	@Test
 	public void testMultiTrades() {
 		this.player1.addValue3(3, Chip.ChipType.COIN);
-
 		TradeHandler.tradeFromToValue3(this.player1, this.player2, 2);
 		TradeHandler.tradeFromToValue3(this.player2, this.player1, 1);
 		TradeHandler.tradeFromToValue1(this.player2, this.player1, 3);
@@ -106,7 +99,6 @@ public class TradeHandlerTest {
 		assertEquals(12, this.player1.getCoinTotal());
 		assertEquals(6, this.player1.getNumValue1Coins());
 		assertEquals(2, this.player1.getNumValue3Coins());
-
 		assertEquals(3, this.player2.getCoinTotal());
 		assertEquals(0, this.player2.getNumValue1Coins());
 		assertEquals(1, this.player2.getNumValue3Coins());
@@ -114,25 +106,17 @@ public class TradeHandlerTest {
 
 	@Test
 	public void testTradeFromTo() {
+		ArrayList<Player> players = setUpArrayWithNumPlayers(3);
+		resetPlayer1And2(players);
 		this.player1.addValue3(2, Chip.ChipType.COIN);
 
-		ArrayList<Player> players = new ArrayList<Player>();
-		players.add(this.player1);
-		players.add(this.player2);
-		players.add(this.player3);
-
-		ArrayList<Card> cards = new SetUpDeckHandler().createCards(Age.AGE1, 3);
-		Deck deck = new Deck(Age.AGE1, cards);
-
-		GameBoard board = new GameBoard(players, deck);
+		GameBoard board = new GameBoard(players, this.testDeck);
 		TradeHandler tradeHandler = new TradeHandler(board);
-
 		tradeHandler.tradeCoinsFromTo(this.player1, this.player2, 4);
 
 		assertEquals(5, this.player1.getCoinTotal());
 		assertEquals(2, this.player1.getNumValue1Coins());
 		assertEquals(1, this.player1.getNumValue3Coins());
-
 		assertEquals(7, this.player2.getCoinTotal());
 		assertEquals(4, this.player2.getNumValue1Coins());
 		assertEquals(1, this.player2.getNumValue3Coins());
@@ -140,19 +124,12 @@ public class TradeHandlerTest {
 
 	@Test
 	public void testTradeFromToMultiValue1SingleValue3() {
+		ArrayList<Player> players = setUpArrayWithNumPlayers(3);
+		resetPlayer1And2(players);
 		this.player1.addValue3(2, Chip.ChipType.COIN);
 
-		ArrayList<Player> players = new ArrayList<Player>();
-		players.add(this.player1);
-		players.add(this.player2);
-		players.add(this.player3);
-
-		ArrayList<Card> cards = new SetUpDeckHandler().createCards(Age.AGE1, 3);
-		Deck deck = new Deck(Age.AGE1, cards);
-
-		GameBoard board = new GameBoard(players, deck);
+		GameBoard board = new GameBoard(players, this.testDeck);
 		TradeHandler tradeHandler = new TradeHandler(board);
-
 		tradeHandler.tradeCoinsFromTo(this.player1, this.player2, 5);
 
 		assertEquals(4, this.player1.getCoinTotal());
@@ -166,19 +143,12 @@ public class TradeHandlerTest {
 
 	@Test
 	public void testTradeFromToSingleValue1MultiValue3() {
+		ArrayList<Player> players = setUpArrayWithNumPlayers(3);
+		resetPlayer1And2(players);
 		this.player1.addValue3(2, Chip.ChipType.COIN);
 
-		ArrayList<Player> players = new ArrayList<Player>();
-		players.add(this.player1);
-		players.add(this.player2);
-		players.add(this.player3);
-
-		ArrayList<Card> cards = new SetUpDeckHandler().createCards(Age.AGE1, 3);
-		Deck deck = new Deck(Age.AGE1, cards);
-
-		GameBoard board = new GameBoard(players, deck);
+		GameBoard board = new GameBoard(players, this.testDeck);
 		TradeHandler tradeHandler = new TradeHandler(board);
-
 		tradeHandler.tradeCoinsFromTo(this.player1, this.player2, 7);
 
 		assertEquals(2, this.player1.getCoinTotal());
@@ -192,19 +162,12 @@ public class TradeHandlerTest {
 
 	@Test
 	public void testTradeFromToMultiValue1MultiValue3() {
+		ArrayList<Player> players = setUpArrayWithNumPlayers(3);
+		resetPlayer1And2(players);
 		this.player1.addValue3(2, Chip.ChipType.COIN);
 
-		ArrayList<Player> players = new ArrayList<Player>();
-		players.add(this.player1);
-		players.add(this.player2);
-		players.add(this.player3);
-
-		ArrayList<Card> cards = new SetUpDeckHandler().createCards(Age.AGE1, 3);
-		Deck deck = new Deck(Age.AGE1, cards);
-
-		GameBoard board = new GameBoard(players, deck);
+		GameBoard board = new GameBoard(players, this.testDeck);
 		TradeHandler tradeHandler = new TradeHandler(board);
-
 		tradeHandler.tradeCoinsFromTo(this.player1, this.player2, 8);
 
 		assertEquals(1, this.player1.getCoinTotal());
@@ -218,17 +181,11 @@ public class TradeHandlerTest {
 
 	@Test
 	public void testTradeFromToSufficientValue1NoValue3() {
-		ArrayList<Player> players = new ArrayList<Player>();
-		players.add(this.player1);
-		players.add(this.player2);
-		players.add(this.player3);
+		ArrayList<Player> players = setUpArrayWithNumPlayers(3);
+		resetPlayer1And2(players);
 
-		ArrayList<Card> cards = new SetUpDeckHandler().createCards(Age.AGE1, 3);
-		Deck deck = new Deck(Age.AGE1, cards);
-
-		GameBoard board = new GameBoard(players, deck);
+		GameBoard board = new GameBoard(players, this.testDeck);
 		TradeHandler tradeHandler = new TradeHandler(board);
-
 		tradeHandler.tradeCoinsFromTo(this.player1, this.player2, 3);
 
 		assertEquals(0, this.player1.getCoinTotal());
@@ -242,19 +199,12 @@ public class TradeHandlerTest {
 
 	@Test
 	public void testTradeFromToGreaterValue1ThanValue3() {
+		ArrayList<Player> players = setUpArrayWithNumPlayers(3);
+		resetPlayer1And2(players);
 		this.player1.addValue3(1, Chip.ChipType.COIN);
 
-		ArrayList<Player> players = new ArrayList<Player>();
-		players.add(this.player1);
-		players.add(this.player2);
-		players.add(this.player3);
-
-		ArrayList<Card> cards = new SetUpDeckHandler().createCards(Age.AGE1, 3);
-		Deck deck = new Deck(Age.AGE1, cards);
-
-		GameBoard board = new GameBoard(players, deck);
+		GameBoard board = new GameBoard(players, this.testDeck);
 		TradeHandler tradeHandler = new TradeHandler(board);
-
 		tradeHandler.tradeCoinsFromTo(this.player1, this.player2, 6);
 
 		assertEquals(0, this.player1.getCoinTotal());
@@ -268,19 +218,11 @@ public class TradeHandlerTest {
 
 	@Test
 	public void testTradeToNextPlayer() {
-		ArrayList<Player> players = new ArrayList<Player>();
-		players.add(this.player1);
-		players.add(this.player2);
-		players.add(this.player3);
-		players.add(this.player4);
-		players.add(this.player5);
+		ArrayList<Player> players = setUpArrayWithNumPlayers(5);
+		resetTestDeck(5);
 
-		ArrayList<Card> cards = new SetUpDeckHandler().createCards(Age.AGE1, 3);
-		Deck deck = new Deck(Age.AGE1, cards);
-
-		GameBoard board = new GameBoard(players, deck);
+		GameBoard board = new GameBoard(players, this.testDeck);
 		TradeHandler tradeHandler = new TradeHandler(board);
-
 		tradeHandler.tradeCoinsFromTo(board.getCurrentPlayer(), board.getNextPlayer(), 3);
 
 		assertEquals(0, board.getPlayerCoinTotal(board.getCurrentPlayerIndex()));
@@ -289,19 +231,11 @@ public class TradeHandlerTest {
 
 	@Test
 	public void testTradeToPreviousPlayer() {
-		ArrayList<Player> players = new ArrayList<Player>();
-		players.add(this.player1);
-		players.add(this.player2);
-		players.add(this.player3);
-		players.add(this.player4);
-		players.add(this.player5);
+		ArrayList<Player> players = setUpArrayWithNumPlayers(5);
+		resetTestDeck(5);
 
-		ArrayList<Card> cards = new SetUpDeckHandler().createCards(Age.AGE1, 3);
-		Deck deck = new Deck(Age.AGE1, cards);
-
-		GameBoard board = new GameBoard(players, deck);
+		GameBoard board = new GameBoard(players, this.testDeck);
 		TradeHandler tradeHandler = new TradeHandler(board);
-
 		tradeHandler.tradeCoinsFromTo(board.getCurrentPlayer(), board.getPreviousPlayer(), 3);
 
 		assertEquals(0, board.getPlayerCoinTotal(board.getCurrentPlayerIndex()));
@@ -310,44 +244,31 @@ public class TradeHandlerTest {
 
 	@Test(expected = InvalidTradeException.class)
 	public void testTradeToInvalidPlayer() {
-		ArrayList<Player> players = new ArrayList<Player>();
-		players.add(this.player1);
-		players.add(this.player2);
-		players.add(this.player3);
-		players.add(this.player4);
-		players.add(this.player5);
+		ArrayList<Player> players = setUpArrayWithNumPlayers(5);
+		resetTestDeck(5);
 
-		ArrayList<Card> cards = new SetUpDeckHandler().createCards(Age.AGE1, 3);
-		Deck deck = new Deck(Age.AGE1, cards);
-
-		GameBoard board = new GameBoard(players, deck);
+		GameBoard board = new GameBoard(players, this.testDeck);
 		TradeHandler tradeHandler = new TradeHandler(board);
-
 		tradeHandler.tradeCoinsFromTo(board.getCurrentPlayer(), board.getPlayer(2), 3);
+
 		fail();
 	}
 
 	@Test
 	public void testValidTrade2CoinsForSingleLumberResource() {
-		ArrayList<Player> players = new ArrayList<Player>();
-		players.add(this.player1);
-		players.add(this.player2);
-		players.add(this.player3);
+		ArrayList<Player> players = setUpArrayWithNumPlayers(3);
+		resetTestDeck(3);
 
-		ArrayList<Card> cards = new SetUpDeckHandler().createCards(Age.AGE1, 3);
-		Deck deck = new Deck(Age.AGE1, cards);
-
-		GameBoard board = new GameBoard(players, deck);
+		GameBoard board = new GameBoard(players, this.testDeck);
 		TradeHandler tradeHandler = new TradeHandler(board);
 
 		Player current = board.getCurrentPlayer();
 		Player next = board.getNextPlayer();
 		ArrayList<Card> storage = new ArrayList<Card>();
-		storage.add(deck.getCard(0));
-		storage.add(deck.getCard(1));
+		storage.add(this.testDeck.getCard(0));
+		storage.add(this.testDeck.getCard(1));
 
 		next.setStoragePile(storage);
-
 		tradeHandler.tradeFromToForEntity(current, next, RawResource.LUMBER, false);
 
 		assertEquals(0, current.getNumValue3Coins());
@@ -357,22 +278,17 @@ public class TradeHandlerTest {
 
 	@Test(expected = InvalidTradeException.class)
 	public void testInvalidTrade2CoinsForSingleLumberResource() {
-		ArrayList<Player> players = new ArrayList<Player>();
-		players.add(this.player1);
-		players.add(this.player2);
-		players.add(this.player3);
+		ArrayList<Player> players = setUpArrayWithNumPlayers(3);
+		resetTestDeck(3);
 
-		ArrayList<Card> cards = new SetUpDeckHandler().createCards(Age.AGE1, 3);
-		Deck deck = new Deck(Age.AGE1, cards);
-
-		GameBoard board = new GameBoard(players, deck);
+		GameBoard board = new GameBoard(players, this.testDeck);
 		TradeHandler tradeHandler = new TradeHandler(board);
 
 		Player current = board.getCurrentPlayer();
 		Player next = board.getNextPlayer();
 		ArrayList<Card> storage = new ArrayList<Card>();
-		storage.add(deck.getCard(0));
-		storage.add(deck.getCard(1));
+		storage.add(this.testDeck.getCard(0));
+		storage.add(this.testDeck.getCard(1));
 
 		next.setStoragePile(storage);
 
@@ -382,25 +298,19 @@ public class TradeHandlerTest {
 
 	@Test
 	public void testValidTrade2CoinsForSingleLoomGood() {
-		ArrayList<Player> players = new ArrayList<Player>();
-		players.add(this.player1);
-		players.add(this.player2);
-		players.add(this.player3);
+		ArrayList<Player> players = setUpArrayWithNumPlayers(3);
+		resetTestDeck(3);
 
-		ArrayList<Card> cards = new SetUpDeckHandler().createCards(Age.AGE1, 3);
-		Deck deck = new Deck(Age.AGE1, cards);
-
-		GameBoard board = new GameBoard(players, deck);
+		GameBoard board = new GameBoard(players, this.testDeck);
 		TradeHandler tradeHandler = new TradeHandler(board);
 
 		Player current = board.getCurrentPlayer();
 		Player next = board.getNextPlayer();
 		ArrayList<Card> storage = new ArrayList<Card>();
-		storage.add(deck.getCard(7));
-		storage.add(deck.getCard(8));
+		storage.add(this.testDeck.getCard(7));
+		storage.add(this.testDeck.getCard(8));
 
 		next.setStoragePile(storage);
-
 		tradeHandler.tradeFromToForEntity(current, next, Good.GLASS, false);
 
 		assertEquals(0, current.getNumValue3Coins());
@@ -410,25 +320,20 @@ public class TradeHandlerTest {
 
 	@Test
 	public void testValidTrade1CoinHasEastTradingPost() {
-		ArrayList<Player> players = new ArrayList<Player>();
-		players.add(this.player1);
-		players.add(this.player2);
-		players.add(this.player3);
+		ArrayList<Player> players = setUpArrayWithNumPlayers(3);
+		resetTestDeck(3);
 
-		ArrayList<Card> cards = new SetUpDeckHandler().createCards(Age.AGE1, 3);
-		Deck deck = new Deck(Age.AGE1, cards);
-
-		GameBoard board = new GameBoard(players, deck);
+		GameBoard board = new GameBoard(players, this.testDeck);
 		TradeHandler tradeHandler = new TradeHandler(board);
 
 		Player current = board.getPlayer(0);
 		Player right = board.getPlayer(1);
 		ArrayList<Card> storage = new ArrayList<Card>();
-		storage.add(deck.getCard(12)); // east trading post
+		storage.add(this.testDeck.getCard(12)); // east trading post
 		current.setStoragePile(storage);
 
 		ArrayList<Card> rStorage = new ArrayList<Card>();
-		rStorage.add(deck.getCard(0));
+		rStorage.add(this.testDeck.getCard(0));
 		right.setStoragePile(rStorage);
 
 		tradeHandler.tradeFromToForEntity(current, right, RawResource.LUMBER, true);
@@ -436,5 +341,23 @@ public class TradeHandlerTest {
 		assertEquals(2, current.getNumValue1Coins());
 		assertEquals(1, (int) current.getCurrentTrades().get(RawResource.LUMBER));
 		assertEquals(4, right.getCoinTotal());
+	}
+
+	private ArrayList<Player> setUpArrayWithNumPlayers(int num) {
+		ArrayList<Player> result = new ArrayList<Player>();
+		for (int i = 0; i < num; i++) {
+			Player temp = new Player("Jane Doe", this.wonder);
+			result.add(temp);
+		}
+		return result;
+	}
+
+	private void resetPlayer1And2(ArrayList<Player> players) {
+		this.player1 = players.get(0);
+		this.player2 = players.get(1);
+	}
+
+	private void resetTestDeck(int numPlayers) {
+		this.testDeck = new SetUpDeckHandler().createDeck(Age.AGE1, numPlayers);
 	}
 }
