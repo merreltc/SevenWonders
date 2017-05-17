f52760b90f46f83fa8008ba5a0f8ffeeed836a32
merreltc <merreltc@rose-hulman.edu>
2017-05-17 00:31:48 -0400

    TDD: refactored SetUpPlayerHandler and Test. all tests pass

--- 7_Wonders/src/test/java/backendTests/SetUpPlayerHandlerTest.java
+++ 7_Wonders/src/test/java/backendTests/SetUpPlayerHandlerTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 
 import backend.GameManager;
 import backend.handlers.SetUpPlayerHandler;
+import constants.GeneralEnums.GameMode;
 import dataStructures.gameMaterials.Wonder.WonderType;
 import dataStructures.playerData.Player;
 
@@ -18,27 +19,26 @@ public class SetUpPlayerHandlerTest {
 
 	@Test
 	public void testValidPlayerNum() {
-		assertTrue(new SetUpPlayerHandler().validatePlayerNum(3));
-		assertTrue(new SetUpPlayerHandler().validatePlayerNum(7));
+		assertTrue(new SetUpPlayerHandler(GameMode.EASY).validatePlayerNum(3));
+		assertTrue(new SetUpPlayerHandler(GameMode.EASY).validatePlayerNum(7));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testInvalidPlayerNum2() {
-		new SetUpPlayerHandler().validatePlayerNum(2);
+		new SetUpPlayerHandler(GameMode.EASY).validatePlayerNum(2);
 		fail();
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testInvalidPlayerNum8() {
-		new SetUpPlayerHandler().validatePlayerNum(8);
+		new SetUpPlayerHandler(GameMode.EASY).validatePlayerNum(8);
 		fail();
 	}
-
 
 	@Test
 	public void testInvalidPlayerNum2ErrorMessage() {
 		try {
-			new SetUpPlayerHandler().validatePlayerNum(2);
+			new SetUpPlayerHandler(GameMode.EASY).validatePlayerNum(2);
 		} catch (IllegalArgumentException error) {
 			String message = "Cannot play with 2 players";
 			assertEquals(message, error.getMessage());
@@ -48,7 +48,7 @@ public class SetUpPlayerHandlerTest {
 	@Test
 	public void testInvalidPlayerNum8ErrorMessage() {
 		try {
-			new SetUpPlayerHandler().validatePlayerNum(8);
+			new SetUpPlayerHandler(GameMode.EASY).validatePlayerNum(8);
 		} catch (IllegalArgumentException error) {
 			String message = "Cannot play with 8 players";
 			assertEquals(message, error.getMessage());
@@ -58,37 +58,18 @@ public class SetUpPlayerHandlerTest {
 	@Test(expected = IllegalArgumentException.class)
 	public void testCreatePlayersZero() {
 		ArrayList<String> playerNames = new ArrayList<String>();
-		ArrayList<WonderType> wonders = new ArrayList<WonderType>();
-
-		GameManager manager = new GameManager(playerNames);
-
-		SetUpPlayerHandler setUpHandler = new SetUpPlayerHandler();
+		SetUpPlayerHandler setUpHandler = new SetUpPlayerHandler(GameMode.EASY);
 		setUpHandler.setUpAndReturnPlayers(playerNames);
 		fail();
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testCreatePlayersTooMany() {
-		ArrayList<String> playerNames = new ArrayList<String>(
-				Arrays.asList("Wolverine", "Captain America", "Black Widow", "Hulk", "Iron Man", "Spider Man", "Thor", "Ultron"));
-		ArrayList<WonderType> wonders = new ArrayList<WonderType>(
-				Arrays.asList(WonderType.COLOSSUS, WonderType.LIGHTHOUSE, WonderType.TEMPLE, WonderType.STATUE,
-						WonderType.MAUSOLEUM, WonderType.GARDENS, WonderType.PYRAMIDS, WonderType.PYRAMIDS));
+		ArrayList<String> playerNames = new ArrayList<String>(Arrays.asList("Wolverine", "Captain America",
+				"Black Widow", "Hulk", "Iron Man", "Spider Man", "Thor", "Ultron"));
 
-		SetUpPlayerHandler setUpHandler = new SetUpPlayerHandler();
+		SetUpPlayerHandler setUpHandler = new SetUpPlayerHandler(GameMode.EASY);
 		setUpHandler.setUpAndReturnPlayers(playerNames);
 		fail();
-	}
-
-	private void verifyPlayersAndWonders(ArrayList<String> playerNames, ArrayList<WonderType> wonders) {
-		SetUpPlayerHandler setUpHandler = new SetUpPlayerHandler();
-		ArrayList<Player> players = setUpHandler.createPlayers(playerNames);
-
-		for (int i = 0; i < playerNames.size(); i++) {
-			String name = players.get(i).getName();
-			assertTrue(playerNames.contains(name));
-
-			assertEquals(wonders.get(i), players.get(i).getWonder().getType());
-		}
 	}
 }
